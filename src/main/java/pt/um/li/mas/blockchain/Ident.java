d9a3063afb376d79aa93ec4e2bb9beba9db4175b
André Diogo <redan.blue27@gmail.com>
2018-02-23 18:58:07 +0000

    Identity based on eliptic curve keypair.

---
+++ src/main/java/pt/um/li/mas/blockchain/Ident.java
@@ -0,0 +1,35 @@
+package pt.um.li.mas.blockchain;
+
+import java.security.*;
+import java.security.spec.ECGenParameterSpec;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Ident {
+    private static Logger LOGGER = Logger.getLogger("Ident");
+    private PrivateKey privateKey;
+    private PublicKey publicKey;
+
+    public Ident() {
+        generateKeyPair();
+    }
+
+    private void generateKeyPair() {
+        try {
+            KeyPairGenerator keygen = KeyPairGenerator.getInstance("ECDSA","BC");
+            SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+            ECGenParameterSpec ecSpec = new ECGenParameterSpec("prime192v1");
+            // Initialize the key generator and generate a KeyPair
+            keygen.initialize(ecSpec, random);   //256 bytes provides an acceptable security level
+            KeyPair keyPair = keygen.generateKeyPair();
+            // Set the public and private keys from the keyPair
+            privateKey = keyPair.getPrivate();
+            publicKey = keyPair.getPublic();
+        } catch (GeneralSecurityException e) {
+            LOGGER.log(Level.SEVERE, e.getMessage());
+            throw new RuntimeException("Keygen problem", e);
+        }
+    }
+
+
+}
