c12a89496f7367c045b5618fe9d62e8c534ded64
André Diogo <redan.blue27@gmail.com>
2018-02-24 19:03:34 +0000

    Extract actual hashing interface out to Crypter. Remove provisory merkle root calculus in favor of actual merkle tree.

---
+++ src/main/java/pt/um/li/mas/blockchain/stringutils/Crypter.java
@@ -0,0 +1,5 @@
+package pt.um.li.mas.blockchain.stringutils;
+
+public interface Crypter {
+  String applyHash(String input);
+}
--- src/main/java/pt/um/li/mas/blockchain/stringutils/StringUtil.java
+++ src/main/java/pt/um/li/mas/blockchain/stringutils/StringUtil.java
@@ -1,37 +1,17 @@
 package pt.um.li.mas.blockchain.stringutils;
 
-import pt.um.li.mas.blockchain.Transaction;
-
-import java.io.UnsupportedEncodingException;
 import java.security.*;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Base64;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
 
 public final class StringUtil {
-  public static Logger LOGGER = Logger.getLogger("StringUtil");
+  private static Logger LOGGER = Logger.getLogger("StringUtil");
+  private static Crypter DEFAULTCRYPTER = new SHA256Encrypter();
 
-    //Applies Sha256 to a string and returns the result.
-    public static String applySha256(String input) {
-        try {
-            MessageDigest digest = MessageDigest.getInstance("SHA-256");
-            //Applies sha256 to our input,
-            byte[] hash = digest.digest(input.getBytes("UTF-8"));
-            StringBuffer hexString = new StringBuffer(); // This will contain hash as hexidecimal
-          for (byte aHash : hash) {
-            String hex = Integer.toHexString(0xff & aHash);
-            if (hex.length() == 1) hexString.append('0');
-            hexString.append(hex);
-          }
-          return hexString.toString();
-        } catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
-          LOGGER.log(Level.SEVERE, e.getMessage());
-          throw new RuntimeException("Apply SHA256 problem", e);
-        }
-    }
+  public static Crypter getDefaultCrypter() {
+    return DEFAULTCRYPTER;
+  }
 
   //Applies ECDSA Signature and returns the result ( as bytes ).
   public static byte[] applyECDSASig(PrivateKey privateKey, String input) {
@@ -67,24 +47,6 @@ public final class StringUtil {
     return Base64.getEncoder().encodeToString(key.getEncoded());
   }
 
-  public static String getMerkleRoot(Transaction[] data, int size) {
-    int count = size;
-    String previousTreeLayer[] = new String[size];
-    for(int i=0; i< size; i++) {
-      previousTreeLayer[i] = data[i].getTransactionId();
-    }
-    String treeLayer[] = previousTreeLayer;
-    while(count > 1) {
-      treeLayer = new String[treeLayer.length-1];
-      for(int i=1; i < previousTreeLayer.length; i++) {
-        treeLayer[i-1]=applySha256(previousTreeLayer[i-1] + previousTreeLayer[i]);
-      }
-      count = treeLayer.length;
-      previousTreeLayer = treeLayer;
-    }
-    String merkleRoot = (treeLayer.length == 1) ? treeLayer[0] : "";
-    return merkleRoot;
-  }
 
   public static String getDifficultyString(int difficulty) {
     return new String(new char[difficulty]).replace('\0', '0'); //Create a string with difficulty * "0"
