9a1c635649630d20f96de13534e8357882b337f2
André Diogo <redan.blue27@gmail.com>
2018-03-11 20:11:09 +0000

    A parser for JSON files with an array of urls.

---
+++ agent/src/main/java/pt/um/lei/masb/agent/net/JSONFileUrlParser.java
@@ -0,0 +1,39 @@
+package pt.um.lei.masb.agent.net;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.lang.reflect.Type;
+import java.net.URL;
+import java.util.Collection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * A parser for JSON files with an array of urls.
+ */
+public class JSONFileUrlParser {
+  private String root;
+  private URL file;
+  private Collection<URL> apis;
+  private static final Logger LOGGER = Logger.getLogger("JSONFileUrlParser");
+
+  public JSONFileUrlParser(String file) {
+    root = file;
+    this.file = getClass().getResource(file);
+  }
+
+  public Collection<URL> parseFromJSON() {
+    Gson g = new Gson();
+    Type collection = new TypeToken<Collection<URL>>(){}.getType();
+    try {
+      return g.fromJson(new FileReader(file.getFile()), collection);
+    } catch (FileNotFoundException e) {
+     LOGGER.log(Level.SEVERE,e.getMessage());
+    }
+    return null;
+  }
+}
