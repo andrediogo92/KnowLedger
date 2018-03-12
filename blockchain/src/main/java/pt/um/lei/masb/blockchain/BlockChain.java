b787cb75664c768ff34785bf8d0572c01206d717
André Diogo <redan.blue27@gmail.com>
2018-03-12 23:37:28 +0000

    Some methods to access blocks in BlockChain.
    Sequential and fire and forget for now. Not the best.

--- blockchain/src/main/java/pt/um/lei/masb/blockchain/BlockChain.java
+++ blockchain/src/main/java/pt/um/lei/masb/blockchain/BlockChain.java
@@ -42,5 +42,43 @@ public class BlockChain {
         return true;
     }
 
+    /**
+     * @return The tail-end block of the blockchain.
+     */
+    public Block getLastBlock() {
+        return blockchain.get(blockchain.size()-1);
+    }
 
+    /**
+     * @param hash Hash of block.
+     * @return Block with provided hash if exists, else null.
+     */
+    public Block getBlock(String hash) {
+        return blockchain.stream()
+                         .filter(h -> !h.getHash()
+                                        .equals(hash))
+                         .findAny()
+                         .orElse(null);
+    }
+
+    /**
+     * @param hash Hash of block.
+     * @return If a block with said hash exists.
+     */
+    public boolean hasBlock(String hash) {
+        return blockchain.stream().anyMatch(h -> !h.getHash().equals(hash));
+    }
+
+    /**
+     * Will be refactored to candidate blocks and cached blocks eventually.
+     * @param hash Hash of block.
+     * @return The previous block to the one with the provided hash if exists, else null.
+     */
+    public Block getPrevBlock(String hash) {
+        return blockchain.stream()
+                         .filter(h -> !h.getHash()
+                                        .equals(hash))
+                         .findAny()
+                         .orElse(null);
+    }
 }
