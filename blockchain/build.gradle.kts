b212d976f9a3c14ab7010c9fe1649e109bc3c359
André Diogo <redan.blue27@gmail.com>
2018-04-20 22:43:56 +0100

    Stabilize Gradle version at 4.7.

--- blockchain/build.gradle.kts
+++ blockchain/build.gradle.kts
@@ -3,6 +3,8 @@ version = "1.0"
 
 //val vertxVersion = "3.5.1"
 val junitversion = "5.1.0"
+val gsonversion = "2.8.2"
+val bouncycastle = "1.59"
 
 plugins {
     `java-library`
@@ -12,8 +14,8 @@ dependencies {
     //compile("io.vertx", "vertx-mongo-client", vertxVersion)
     testCompile("org.junit.jupiter", "junit-jupiter-api", junitversion)
     testRuntime("org.junit.jupiter", "junit-jupiter-params", junitversion)
-    compile("com.google.code.gson", "gson", "2.8.2")
-    implementation("org.bouncycastle", "bcprov-jdk15on", "1.59")
+    compile("com.google.code.gson", "gson", gsonversion)
+    implementation("org.bouncycastle", "bcprov-jdk15on", bouncycastle)
 }
 
 tasks.withType<JavaCompile> {
