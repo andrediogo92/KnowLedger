a9960be542c6fd2ec9e64ba43017a2e8e3c7b18d
André Diogo <redan.blue27@gmail.com>
2018-05-03 12:58:05 +0100

    Makes the Blockchain immutable and single row table.

--- blockchain/src/main/java/pt/um/lei/masb/blockchain/BlockChain.java
+++ blockchain/src/main/java/pt/um/lei/masb/blockchain/BlockChain.java
@@ -1,42 +1,52 @@
 package pt.um.lei.masb.blockchain;
 
+import pt.um.lei.masb.blockchain.persistance.PersistanceWrapper;
 import pt.um.lei.masb.blockchain.utils.RingBuffer;
 import pt.um.lei.masb.blockchain.utils.StringUtil;
 
+import javax.persistence.Basic;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.validation.constraints.DecimalMax;
-import javax.validation.constraints.DecimalMin;
+import javax.persistence.Transient;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.*;
-import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.util.Optional;
 
 @Entity
 public final class BlockChain {
-    @Id
-    @GeneratedValue
-    private long id;
+    private static final BlockChain blockChain =
+            PersistanceWrapper.getInstance()
+                              .executeInSessionAndReturn(
+                                      t -> Optional.ofNullable(
+                                              t.find(BlockChain.class,
+                                                     1))
+                                                   .orElse(new BlockChain()));
+
+    static {
+        PersistanceWrapper.getInstance()
+                          .closeCurrentSession();
+    }
 
     private static final int CACHE_SIZE = 40;
     private static final int RECALC_TRIGGER = 2048;
 
+    @Id
+    private final long id = 1;
+    @Transient
     private transient final RingBuffer<Block> blockchain;
-    private BigInteger difficultyTarget;
-    private int lastRecalc;
+    @Basic(optional = false)
     private final boolean bounded;
-    private final BigDecimal boundnorth;
-    private final BigDecimal boundsouth;
-    private final BigDecimal boundeast;
-    private final BigDecimal boundwest;
-    // private final List<Block> candidateBlocks;
+    @Basic(optional = false)
+    private BigInteger difficultyTarget;
+
+    @Basic(optional = false)
+    private int lastRecalc;
 
     /**
      * Create a geographically unbounded blockchain.
      */
-    public BlockChain() {
+    protected BlockChain() {
         this.blockchain = new RingBuffer<>(CACHE_SIZE);
         //    this.candidateBlocks = new ArrayList<>(CACHE_SIZE);
         var origin = Block.getOrigin();
@@ -44,63 +54,19 @@ public final class BlockChain {
         difficultyTarget = StringUtil.getInitialDifficulty();
         lastRecalc = 0;
         bounded = false;
-        boundnorth = null;
-        boundsouth = null;
-        boundeast = null;
-        boundwest = null;
+    }
+
+    public static BlockChain getInstance() {
+        return blockChain;
     }
 
     /**
-     * Create a geographically bounded blockchain.
-     * @param boundnorth A northern latitude bound in the [0, 90] interval.
-     * @param boundsouth A southern latitude bound in the [0, -90] interval.
-     * @param boundeast An eastern longitude bound in the [0, 180] interval.
-     * @param boundwest A western longitude bound in the [0, -180] interval.
+     * @return The current calculated difficulty target.
      */
-    public BlockChain(@DecimalMin(value = "0")
-                      @DecimalMax(value = "90")
-                              BigDecimal boundnorth,
-                      @DecimalMin(value = "-90")
-                      @DecimalMax(value = "0")
-                              BigDecimal boundsouth,
-                      @DecimalMin(value = "0")
-                      @DecimalMax(value = "180")
-                              BigDecimal boundeast,
-                      @DecimalMin(value = "-180")
-                      @DecimalMax(value = "0")
-                              BigDecimal boundwest) {
-        this.blockchain = new RingBuffer<>(CACHE_SIZE);
-        //    this.candidateBlocks = new ArrayList<>(CACHE_SIZE);
-        var origin = Block.getOrigin();
-        blockchain.offer(origin);
-        difficultyTarget = StringUtil.getInitialDifficulty();
-        lastRecalc = 0;
-        bounded = true;
-        this.boundnorth = boundnorth;
-        this.boundsouth = boundsouth;
-        this.boundeast = boundeast;
-        this.boundwest = boundwest;
+    public BigInteger getDifficultyTarget() {
+        return difficultyTarget;
     }
 
-    /**
-     * Returns the bounds if the blockchain is bounded.
-     *
-     * @return the bounds of the blockchain in each cardinal direction [N, S, E, W].
-     * <p> Possible values are [[0,90],[-90,0],[0,180],[-180,0]].
-     * <p> If the blockchain is not bounded, returns an empty array.
-     */
-    public @Size(max = 4) BigDecimal[] getBounds() {
-        if (bounded) {
-            return new BigDecimal[]{
-                    boundnorth,
-                    boundsouth,
-                    boundeast,
-                    boundwest
-            };
-        } else {
-            return new BigDecimal[0];
-        }
-    }
 
     public boolean isBounded() {
         return bounded;
