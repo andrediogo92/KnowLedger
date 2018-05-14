6d3022ea3cc4a9d7f2349b73c09427b5ae857e78
André Diogo <redan.blue27@gmail.com>
2018-05-14 02:34:35 +0100

    Makes reference previous coinbase hash from which initial payout is derived.

--- blockchain/src/main/java/pt/um/lei/masb/blockchain/TransactionOutput.java
+++ blockchain/src/main/java/pt/um/lei/masb/blockchain/TransactionOutput.java
@@ -2,9 +2,12 @@ package pt.um.lei.masb.blockchain;
 
 import org.openjdk.jol.info.ClassLayout;
 
+import javax.persistence.Basic;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.math.BigDecimal;
+import java.security.PublicKey;
 
 @Entity
 public class TransactionOutput implements Sizeable {
@@ -12,8 +15,40 @@ public class TransactionOutput implements Sizeable {
     @GeneratedValue
     private long id;
 
+    @Basic(optional = false)
+    private final PublicKey publicKey;
+
+    @Basic(optional = false)
+    private final String prevUTXO;
+
+    @Basic(optional = false)
+    private BigDecimal payout;
+
+    protected TransactionOutput() {
+        publicKey = null;
+        prevUTXO = null;
+    }
+
+    protected TransactionOutput(PublicKey publicKey, String prevUTXO, BigDecimal cumUTXO) {
+        this.publicKey = publicKey;
+        this.prevUTXO = prevUTXO;
+        payout = cumUTXO;
+    }
+
     @Override
     public long getApproximateSize() {
         return ClassLayout.parseClass(this.getClass()).instanceSize();
+    }
+
+    public PublicKey getPublicKey() {
+        return publicKey;
+    }
+
+    public BigDecimal getPayout() {
+        return payout;
+    }
+
+    public void addToPayout(BigDecimal payout) {
+        this.payout.add(payout);
     }
 }
