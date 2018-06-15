f9c6be2f0f5d2a5b76337249d37722fc78c92948
André Diogo <redan.blue27@gmail.com>
2018-06-15 13:44:37 +0100

    Keeps a Set of concatenated previous transaction hash + new transaction hash.
    Makes Transaction output Hashed, with hash recalculation on every new
    payout added.

--- blockchain/src/main/java/pt/um/lei/masb/blockchain/TransactionOutput.java
+++ blockchain/src/main/java/pt/um/lei/masb/blockchain/TransactionOutput.java
@@ -1,17 +1,18 @@
 package pt.um.lei.masb.blockchain;
 
-import org.openjdk.jol.info.ClassLayout;
+import org.openjdk.jol.info.GraphLayout;
+import pt.um.lei.masb.blockchain.utils.StringUtil;
 
-import javax.persistence.Basic;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.security.PublicKey;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
-public class TransactionOutput implements Sizeable {
+public class TransactionOutput implements Sizeable, Hashed {
     @Id
     @GeneratedValue
     private long id;
@@ -20,27 +21,54 @@ public class TransactionOutput implements Sizeable {
     private final PublicKey publicKey;
 
     @Basic(optional = false)
-    private final String prevUTXO;
+    private final String prevCoinbase;
+    @Basic(optional = false)
+    private String hashId;
 
     @Basic(optional = false)
     private BigDecimal payout;
+    @ElementCollection
+    private Set<String> tx;
 
     protected TransactionOutput() {
         publicKey = null;
-        prevUTXO = null;
+        prevCoinbase = null;
     }
 
     public TransactionOutput(@NotNull PublicKey publicKey,
-                             @NotNull String prevUTXO,
-                             @NotNull BigDecimal cumUTXO) {
+                             @NotNull String hashId,
+                             @NotNull String prevCoinbase,
+                             @NotNull BigDecimal cumUTXO,
+                             @NotEmpty Set<String> tx) {
         this.publicKey = publicKey;
-        this.prevUTXO = prevUTXO;
+        this.hashId = hashId;
+        this.prevCoinbase = prevCoinbase;
         payout = cumUTXO;
+        this.tx = tx;
+    }
+
+    TransactionOutput(@NotNull PublicKey publicKey,
+                      @NotNull String prevCoinbase,
+                      @NotNull BigDecimal cumUTXO,
+                      @NotNull String newT,
+                      @NotNull String prev) {
+        this.publicKey = publicKey;
+        this.prevCoinbase = prevCoinbase;
+        this.tx = new HashSet<>();
+        payout = new BigDecimal("0");
+        addToPayout(cumUTXO, newT, prev);
+    }
+
+    private String recalculateHash() {
+        return StringUtil.getDefaultCrypter()
+                         .applyHash(tx.stream()
+                                      .reduce("",
+                                              String::concat) + StringUtil.getStringFromKey(publicKey));
     }
 
     @Override
     public long getApproximateSize() {
-        return ClassLayout.parseClass(this.getClass()).instanceSize();
+        return GraphLayout.parseInstance(this).totalSize();
     }
 
     public PublicKey getPublicKey() {
@@ -51,11 +79,27 @@ public class TransactionOutput implements Sizeable {
         return payout;
     }
 
-    public void addToPayout(@NotNull BigDecimal payout) {
-        this.payout.add(payout);
+    public void addToPayout(@NotNull BigDecimal payout,
+                            @NotNull String tx,
+                            @NotNull String prev) {
+        this.tx.add(prev + tx);
+        this.payout = this.payout.add(payout);
+        hashId = recalculateHash();
     }
 
-    public String getPrevUTXO() {
-        return prevUTXO;
+    public String getPrevCoinbase() {
+        return prevCoinbase;
+    }
+
+    public Set<String> getTx() {
+        return tx;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getHashId() {
+        return hashId;
     }
 }
