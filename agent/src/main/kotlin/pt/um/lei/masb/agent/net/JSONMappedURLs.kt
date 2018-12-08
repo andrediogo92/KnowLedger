71e28d4b48983cfdc41211c3509d9b9ddea54bce
André Diogo <redan.blue27@gmail.com>
2018-12-08 16:07:40 +0000

    Refactors network classes to Kotlin.
    Uses JSON serializability to gather data sources.
    
    HTTP Runner runs queries over coroutine IO via Ktor client.

---
+++ agent/src/main/kotlin/pt/um/lei/masb/agent/net/JSONMappedURLs.kt
@@ -0,0 +1,54 @@
+package pt.um.lei.masb.agent.net
+
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JSON
+import mu.KLogging
+import pt.um.lei.masb.agent.Container
+import pt.um.lei.masb.agent.data.DataSource
+import java.io.File
+import java.io.FileReader
+import java.io.InputStreamReader
+import java.net.URI
+
+
+@Serializable
+data class JSONMappedURLs(
+    val apis: List<DataSource> = emptyList(),
+    val tcp: List<DataSource> = emptyList(),
+    val jade: List<DataSource> = emptyList()
+) {
+    companion object : KLogging() {
+        val apiScheme = Regex.fromLiteral("http(s)?")
+        val tcpScheme = Regex.fromLiteral("tcp")
+        val jadeScheme = Regex.fromLiteral("jade")
+
+        /**
+         * A parser for a file based on a path string from the classpath resources.
+         *
+         * @param file the path string to a resource in the classpath.
+         */
+        fun JSONFileUrlParser(file: String): JSONMappedURLs =
+            InputStreamReader(
+                Container::class.java
+                    .getResourceAsStream(file)
+            )
+                .use {
+                    JSON.parse(this.serializer(), it.readText())
+                }
+
+        /**
+         * A parser for a file at the specified URI.
+         *
+         * @param file
+         */
+        fun JSONFileUrlParser(file: URI): JSONMappedURLs =
+            FileReader(File(file))
+                .use {
+                    JSON.parse(this.serializer(), it.readText())
+                }
+
+
+    }
+
+
+}
