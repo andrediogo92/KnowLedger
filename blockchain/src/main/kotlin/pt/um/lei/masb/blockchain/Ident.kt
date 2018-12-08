2777bf1c4fa6734e12e855f8b454c1598f9aac71
André Diogo <redan.blue27@gmail.com>
2018-12-08 08:34:56 +0000

    Ident must be singleton as only library user has a private key accessible.

--- blockchain/src/main/kotlin/pt/um/lei/masb/blockchain/Ident.kt
+++ blockchain/src/main/kotlin/pt/um/lei/masb/blockchain/Ident.kt
@@ -1,7 +1,11 @@
 package pt.um.lei.masb.blockchain
 
 import mu.KLogging
+import org.bouncycastle.jce.provider.BouncyCastleProvider
 import pt.um.lei.masb.blockchain.Ident.KeyGenerationException
+import pt.um.lei.masb.blockchain.persistance.IDENT
+import pt.um.lei.masb.blockchain.utils.stringToPrivateKey
+import pt.um.lei.masb.blockchain.utils.stringToPublicKey
 import java.security.GeneralSecurityException
 import java.security.KeyPairGenerator
 import java.security.PrivateKey
@@ -13,38 +17,80 @@ import java.security.spec.ECGenParameterSpec
 /**
  * @throws KeyGenerationException When key generation fails.
  */
-class Ident {
-    companion object : KLogging() {
-        //Ensure Bouncy Castle Crypto provider is present
-        init {
-            if (Security.getProvider("BC") == null) {
-                Security.addProvider(org.bouncycastle.jce.provider.BouncyCastleProvider())
-            }
-        }
-    }
-
-    class KeyGenerationException(message: String, cause: Exception) : Exception(message, cause)
+object Ident : KLogging() {
 
 
-    val privateKey: PrivateKey
+    lateinit var privateKey: PrivateKey
+        private set
 
-    val publicKey: PublicKey
+
+    lateinit var publicKey: PublicKey
+        private set
+
 
     init {
-        try {
-            val keygen = KeyPairGenerator.getInstance("ECDSA", "BC")
-            val random = SecureRandom.getInstance("SHA1PRNG")
-            val ecSpec = ECGenParameterSpec("prime192v1")
-            // Initialize the key generator and generate a KeyPair
-            keygen.initialize(ecSpec, random)
-            //256 bytes provides an acceptable security level
-            val keyPair = keygen.generateKeyPair()
-            // Set the public and private keys from the keyPair
-            privateKey = keyPair.private
-            publicKey = keyPair.public
-        } catch (e: GeneralSecurityException) {
-            logger.error("", e.message)
-            throw KeyGenerationException("Keygen problem", e)
+        //Ensure Bouncy Castle Crypto provider is present
+        if (Security.getProvider("BC") == null) {
+            Security.addProvider(
+                BouncyCastleProvider()
+            )
+        }
+        if (!loadFromDB()) {
+            try {
+                generateNewIdent()
+            } catch (e: GeneralSecurityException) {
+                logger.error(e) {}
+                throw KeyGenerationException(
+                    "Keygen problem",
+                    e
+                )
+            }
+
         }
     }
+
+
+    private fun loadFromDB(): Boolean = let {
+        val id = IDENT
+        if (id == null) {
+            false
+        } else {
+            privateKey =
+                    stringToPrivateKey(
+                        id.getProperty("privateKey")
+                    )
+            publicKey =
+                    stringToPublicKey(
+                        id.getProperty("publicKey")
+                    )
+            true
+        }
+    }
+
+
+    private fun generateNewIdent() {
+        val keygen = KeyPairGenerator.getInstance(
+            "ECDSA",
+            "BC"
+        )
+        val random = SecureRandom.getInstance(
+            "SHA256PRNG"
+        )
+        val ecSpec = ECGenParameterSpec(
+            "prime512v1"
+        )
+        // Initialize the key generator and generate a KeyPair
+        keygen.initialize(ecSpec, random)
+        //256 bytes provides an acceptable security level
+        val keyPair = keygen.generateKeyPair()
+        // Set the public and private keys from the keyPair
+        privateKey = keyPair.private
+        publicKey = keyPair.public
+
+    }
+
+    class KeyGenerationException(
+        message: String,
+        cause: Exception
+    ) : Exception(message, cause)
 }
