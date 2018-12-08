28f07bf73ac24457dd1bf02f18bb754766453303
André Diogo <redan.blue27@gmail.com>
2018-12-08 08:08:43 +0000

    Adds altitude which defaults to 0 if unspecified.

--- blockchain/src/main/kotlin/pt/um/lei/masb/blockchain/utils/GeoCoords.kt
+++ blockchain/src/main/kotlin/pt/um/lei/masb/blockchain/utils/GeoCoords.kt
@@ -2,29 +2,19 @@ package pt.um.lei.masb.blockchain.utils
 
 import java.math.BigDecimal
 
-class GeoCoords(
-        val latitude: BigDecimal,
-        val longitude: BigDecimal
+data class GeoCoords(
+    val latitude: BigDecimal,
+    val longitude: BigDecimal,
+    val altitude: BigDecimal = BigDecimal.ZERO
 ) {
 
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other !is GeoCoords) return false
-
-        if (latitude != other.latitude) return false
-        if (longitude != other.longitude) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = latitude.hashCode()
-        result = 31 * result + longitude.hashCode()
-        return result
-    }
-
-    override fun toString(): String =
-            "GeoCoords{latitude=$latitude, longitude=$longitude}"
+    override fun toString(): String = """
+        | GeoCoords{
+        |   latitude=$latitude,
+        |   longitude=$longitude,
+        |   altitude=$altitude
+        | }
+        """.trimMargin()
 
 
 }
