8a8c4203eb15cde847f94ded57702bf7357526f1
André Diogo <redan.blue27@gmail.com>
2018-12-22 14:22:11 +0000

    Makes blockchain data hashable.

--- blockchain/src/main/kotlin/pt/um/lei/masb/blockchain/data/BlockChainData.kt
+++ blockchain/src/main/kotlin/pt/um/lei/masb/blockchain/data/BlockChainData.kt
@@ -1,10 +1,13 @@
 package pt.um.lei.masb.blockchain.data
 
 import pt.um.lei.masb.blockchain.Sizeable
+import pt.um.lei.masb.blockchain.persistance.Storable
+import pt.um.lei.masb.blockchain.utils.Hashable
 import java.io.Serializable
 
 interface BlockChainData : SelfInterval,
                            DataCategory,
                            Sizeable,
+                           Hashable,
                            Serializable,
                            Storable
