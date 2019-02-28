72dfc66bfcfb4f93e0c424967be71743da4518f4
André Diogo <redan.blue27@gmail.com>
2019-02-28 15:33:48 +0000

    Isolates ident as simple data class.

--- blockchain/src/main/kotlin/pt/um/lei/masb/blockchain/Ident.kt
+++ blockchain/src/main/kotlin/pt/um/lei/masb/blockchain/Ident.kt
@@ -5,8 +5,7 @@ import mu.KLogging
 import org.bouncycastle.jce.provider.BouncyCastleProvider
 import pt.um.lei.masb.blockchain.persistance.NewInstanceSession
 import pt.um.lei.masb.blockchain.persistance.Storable
-import pt.um.lei.masb.blockchain.utils.stringToPrivateKey
-import pt.um.lei.masb.blockchain.utils.stringToPublicKey
+import java.security.KeyPair
 import java.security.KeyPairGenerator
 import java.security.PrivateKey
 import java.security.PublicKey
@@ -14,44 +13,20 @@ import java.security.SecureRandom
 import java.security.Security
 import java.security.spec.ECGenParameterSpec
 
-object Ident :
-    KLogging(),
-    Storable,
+data class Ident(
+    val id: String,
+    private val pair: KeyPair
+) : Storable,
     BlockChainContract {
 
 
-    lateinit var privateKey: PrivateKey
-        private set
-
-    lateinit var publicKey: PublicKey
-        private set
-
-    private val keygen = KeyPairGenerator.getInstance(
-        "ECDSA",
-        "BC"
-    )
-
-    private val random = SecureRandom.getInstanceStrong()
-
-    private val ecSpec = ECGenParameterSpec(
-        "P-521"
-    )
+    val privateKey: PrivateKey
+        get() = pair.private
+    val publicKey: PublicKey
+        get() = pair.public
 
 
-    init {
-        //Ensure Bouncy Castle Crypto provider is present
-        if (Security.getProvider("BC") == null) {
-            Security.addProvider(
-                BouncyCastleProvider()
-            )
-        }
-        if (!loadFromDB()) {
-            val (prKey, pubKey) = generateNewIdent()
-            privateKey = prKey
-            publicKey = pubKey
-
-        }
-    }
+    constructor(id: String) : this(id, generateNewKeyPair())
 
 
     override fun store(
@@ -60,6 +35,10 @@ object Ident :
         session
             .newInstance("Ident")
             .apply {
+                this.setProperty(
+                    "id",
+                    id
+                )
                 this.setProperty(
                     "publicKey",
                     publicKey.encoded
@@ -71,29 +50,39 @@ object Ident :
             }
 
 
-    private fun loadFromDB(): Boolean = let {
-        val id: OElement? = null//ident
-        if (id == null) {
-            false
-        } else {
-            privateKey =
-                    stringToPrivateKey(
-                        id.getProperty("privateKey")
-                    )
-            publicKey =
-                    stringToPublicKey(
-                        id.getProperty("publicKey")
-                    )
-            true
+    companion object : KLogging() {
+        init {
+            //Ensure Bouncy Castle Crypto provider is present
+            if (Security.getProvider("BC") == null) {
+                Security.addProvider(
+                    BouncyCastleProvider()
+                )
+            }
         }
-    }
 
-    fun generateNewIdent(): Pair<PrivateKey, PublicKey> {
-        // Initialize the key generator and generate a KeyPair
-        keygen.initialize(ecSpec, random)
-        //256 bytes provides an acceptable security level
-        val keyPair = keygen.generateKeyPair()
-        // Set the public and private keys from the keyPair
-        return Pair(keyPair.private, keyPair.public)
+        private val keygen by lazy {
+            KeyPairGenerator.getInstance(
+                "ECDSA",
+                "BC"
+            )
+        }
+
+        private val random by lazy {
+            SecureRandom.getInstanceStrong()
+        }
+
+        private val ecSpec by lazy {
+            ECGenParameterSpec(
+                "P-521"
+            )
+        }
+
+        private fun generateNewKeyPair(): KeyPair {
+            // Initialize the key generator and generate a KeyPair
+            keygen.initialize(ecSpec, random)
+            //256 bytes provides an acceptable security level
+            return keygen.generateKeyPair()
+        }
+
     }
 }
