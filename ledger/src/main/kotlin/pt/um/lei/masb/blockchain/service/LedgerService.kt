d15aad511bbbee6f9316f1fb14598452452ad6f9
André Diogo <redan.blue27@gmail.com>
2019-03-18 23:51:18 +0000

    Subsumes LoaderManager into LedgerService.

--- ledger/src/main/kotlin/pt/um/lei/masb/blockchain/service/LedgerService.kt
+++ ledger/src/main/kotlin/pt/um/lei/masb/blockchain/service/LedgerService.kt
@@ -1,15 +1,22 @@
 package pt.um.lei.masb.blockchain.service
 
 import com.orientechnologies.orient.core.record.OElement
+import mu.KLogging
+import pt.um.lei.masb.blockchain.data.BlockChainData
+import pt.um.lei.masb.blockchain.data.Loadable
 import pt.um.lei.masb.blockchain.ledger.Hash
 import pt.um.lei.masb.blockchain.ledger.LedgerId
 import pt.um.lei.masb.blockchain.persistance.ManagedDatabase
 import pt.um.lei.masb.blockchain.persistance.ManagedDatabaseInfo
 import pt.um.lei.masb.blockchain.persistance.PersistenceWrapper
 import pt.um.lei.masb.blockchain.persistance.PluggableDatabase
+import pt.um.lei.masb.blockchain.persistance.loaders.Loaders
+import pt.um.lei.masb.blockchain.persistance.loaders.PluggableLoaders
 import pt.um.lei.masb.blockchain.service.results.LedgerResult
 import pt.um.lei.masb.blockchain.utils.Crypter
 import pt.um.lei.masb.blockchain.utils.DEFAULT_CRYPTER
+import pt.um.lei.masb.blockchain.utils.base64encode
+import pt.um.lei.masb.blockchain.utils.intoLedger
 import pt.um.lei.masb.blockchain.utils.stringToPrivateKey
 import pt.um.lei.masb.blockchain.utils.stringToPublicKey
 import java.security.KeyPair
@@ -43,12 +50,71 @@ class LedgerService(
     //Get a specific blockchain from DB
     fun getLedgerHandleById(
         id: LedgerId
-    ): LedgerResult =
+    ): LedgerResult<LedgerHandle> =
         pw.getBlockChain(id)
 
     fun getLedgerHandleByHash(
+        crypterHash: Hash = DEFAULT_CRYPTER.id,
         hash: Hash
-    ): LedgerResult =
-        pw.getBlockChain(hash)
+    ): LedgerResult<LedgerHandle> =
+        pw.getBlockChain(crypterHash, hash)
 
+    fun newLedgerHandle(
+        id: String
+    ): LedgerResult<LedgerHandle> {
+        val ledgerHandle = LedgerHandle(pw, id)
+        return pw.persistEntity(ledgerHandle).intoLedger {
+            ledgerHandle
+        }
+    }
+
+
+    companion object : KLogging() {
+        val crypters = mutableMapOf(
+            base64encode(DEFAULT_CRYPTER.id) to DEFAULT_CRYPTER
+        )
+
+        private val dataLoaders: MutableMap<Hash, Loaders> =
+            mutableMapOf()
+
+
+        @Suppress("UNCHECKED_CAST")
+        internal fun getFromLoaders(
+            blockChainId: Hash,
+            id: String
+        ): Loadable<BlockChainData>? =
+            dataLoaders[blockChainId]
+                ?.loaders
+                ?.get(id) as Loadable<BlockChainData>
+
+        fun <T : BlockChainData> registerLoader(
+            blockChainId: Hash,
+            loaderType: String,
+            loader: Loadable<T>
+        ) {
+            if (dataLoaders.containsKey(blockChainId)) {
+                dataLoaders[blockChainId]!!.loaders +=
+                    (loaderType to loader)
+            } else {
+                dataLoaders[blockChainId] =
+                    PluggableLoaders(
+                        mutableMapOf(
+                            loaderType to loader
+                        )
+                    )
+            }
+        }
+
+        fun registerLoaders(
+            blockChainId: Hash,
+            loaders: Loaders
+        ) {
+            if (dataLoaders.containsKey(blockChainId)) {
+                dataLoaders[blockChainId]!!.loaders +=
+                    loaders.loaders
+            } else {
+                dataLoaders[blockChainId] = loaders
+            }
+        }
+    }
 }
