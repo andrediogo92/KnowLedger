60b585adb25fb12d08f8436b68d20299cbd357b3
André Diogo <redan.blue27@gmail.com>
2019-03-19 00:09:29 +0000

    Adds helpers to pair hashes in tree building.

--- ledger/src/test/kotlin/pt/um/lei/masb/test/utils/Helpers.kt
+++ ledger/src/test/kotlin/pt/um/lei/masb/test/utils/Helpers.kt
@@ -1,14 +1,20 @@
 package pt.um.lei.masb.test.utils
 
+import assertk.fail
 import mu.KLogger
 import pt.um.lei.masb.blockchain.data.PhysicalData
 import pt.um.lei.masb.blockchain.data.TUnit
 import pt.um.lei.masb.blockchain.data.TemperatureData
 import pt.um.lei.masb.blockchain.ledger.Coinbase
+import pt.um.lei.masb.blockchain.ledger.LedgerContract
 import pt.um.lei.masb.blockchain.ledger.Transaction
 import pt.um.lei.masb.blockchain.ledger.TransactionOutput
 import pt.um.lei.masb.blockchain.ledger.emptyHash
 import pt.um.lei.masb.blockchain.service.Ident
+import pt.um.lei.masb.blockchain.service.ServiceHandle
+import pt.um.lei.masb.blockchain.service.results.LedgerResult
+import pt.um.lei.masb.blockchain.service.results.LoadListResult
+import pt.um.lei.masb.blockchain.service.results.LoadResult
 import pt.um.lei.masb.blockchain.utils.Crypter
 import pt.um.lei.masb.blockchain.utils.SHA256Encrypter
 import java.math.BigDecimal
@@ -146,3 +152,101 @@ internal fun logActualToExpectedLists(
         """.trimMargin()
     }
 }
+
+internal fun applyHashInPairs(
+    crypter: Crypter,
+    hashes: Array<ByteArray>
+): ByteArray {
+    var previousHashes = hashes
+    var newHashes: Array<ByteArray>
+    var levelIndex = hashes.size
+    while (levelIndex > 2) {
+        if (levelIndex % 2 == 0) {
+            levelIndex /= 2
+            newHashes = Array(levelIndex) {
+                crypter.applyHash(
+                    previousHashes[it * 2] + previousHashes[it * 2 + 1]
+                )
+            }
+        } else {
+            levelIndex /= 2
+            levelIndex++
+            newHashes = Array(levelIndex) {
+                if (it != levelIndex - 1) {
+                    crypter.applyHash(
+                        previousHashes[it * 2] + previousHashes[it * 2 + 1]
+                    )
+                } else {
+                    crypter.applyHash(
+                        previousHashes[it * 2] + previousHashes[it * 2]
+                    )
+                }
+            }
+        }
+        previousHashes = newHashes
+    }
+    return crypter.applyHash(previousHashes[0] + previousHashes[1])
+}
+
+internal inline fun <T : LedgerContract> LoadListResult<T>.applyOrFail(
+    block: List<T>.() -> Unit
+) {
+    when (this) {
+        is LoadListResult.Success -> this.data.block()
+        is LoadListResult.QueryFailure ->
+            if (exception != null)
+                org.junit.jupiter.api.fail(cause, exception)
+            else
+                fail(cause)
+        is LoadListResult.NonExistentData -> fail(cause)
+        is LoadListResult.NonMatchingCrypter -> fail(cause)
+        is LoadListResult.UnregisteredCrypter -> fail(cause)
+        is LoadListResult.UnrecognizedDataType -> fail(cause)
+    }
+}
+
+internal inline fun <T : LedgerContract> LoadResult<T>.applyOrFail(
+    block: T.() -> Unit
+) {
+    when (this) {
+        is LoadResult.Success -> this.data.block()
+        is LoadResult.QueryFailure ->
+            if (exception != null)
+                org.junit.jupiter.api.fail(cause, exception)
+            else
+                fail(cause)
+        is LoadResult.NonExistentData -> fail(cause)
+        is LoadResult.NonMatchingCrypter -> fail(cause)
+        is LoadResult.UnregisteredCrypter -> fail(cause)
+        is LoadResult.UnrecognizedDataType -> fail(cause)
+    }
+}
+
+internal inline fun <T : ServiceHandle> LedgerResult<T>.applyOrFail(
+    block: T.() -> Unit
+) {
+    when (this) {
+        is LedgerResult.Success -> this.data.block()
+        is LedgerResult.QueryFailure ->
+            if (exception != null)
+                org.junit.jupiter.api.fail(cause, exception)
+            else
+                fail(cause)
+        is LedgerResult.NonExistentData -> fail(cause)
+        is LedgerResult.NonMatchingCrypter -> fail(cause)
+        is LedgerResult.UnregisteredCrypter -> fail(cause)
+    }
+}
+
+internal fun <T : ServiceHandle> LedgerResult<T>.extractOrFail(): T =
+    when (this) {
+        is LedgerResult.Success -> this.data
+        is LedgerResult.QueryFailure ->
+            if (exception != null)
+                org.junit.jupiter.api.fail(cause, exception)
+            else
+                fail(cause)
+        is LedgerResult.NonExistentData -> fail(cause)
+        is LedgerResult.NonMatchingCrypter -> fail(cause)
+        is LedgerResult.UnregisteredCrypter -> fail(cause)
+    }
