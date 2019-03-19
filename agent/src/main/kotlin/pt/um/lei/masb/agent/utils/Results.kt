2e6b006bc4579b8c3a9e71365e84d4cdb073fee0
André Diogo <redan.blue27@gmail.com>
2019-03-19 00:18:13 +0000

    Adds top-level result unpacking methods.

---
+++ agent/src/main/kotlin/pt/um/lei/masb/agent/utils/Results.kt
@@ -0,0 +1,24 @@
+package pt.um.lei.masb.agent.utils
+
+import pt.um.lei.masb.blockchain.service.ServiceHandle
+import pt.um.lei.masb.blockchain.service.results.LedgerResult
+
+fun <T : ServiceHandle> LedgerResult<T>.unpackOrThrow(): T =
+    when (this) {
+        is LedgerResult.Success -> data
+        is LedgerResult.NonExistentData -> throw RuntimeException(cause)
+        is LedgerResult.QueryFailure -> throw exception ?: RuntimeException(cause)
+        is LedgerResult.NonMatchingCrypter -> throw RuntimeException(cause)
+        is LedgerResult.UnregisteredCrypter -> throw RuntimeException(cause)
+    }
+
+inline fun <T : ServiceHandle> LedgerResult<T>.unpackOrThrowAndDoOnNonExistent(
+    attempt: LedgerResult.NonExistentData<T>.() -> T
+): T =
+    when (this) {
+        is LedgerResult.Success -> data
+        is LedgerResult.NonExistentData -> attempt()
+        is LedgerResult.QueryFailure -> throw exception ?: RuntimeException(cause)
+        is LedgerResult.NonMatchingCrypter -> throw RuntimeException(cause)
+        is LedgerResult.UnregisteredCrypter -> throw RuntimeException(cause)
+    }
