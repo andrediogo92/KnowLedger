ebae54968c99a77203184168a80a2728ebf57f78
André Diogo <redan.blue27@gmail.com>
2019-04-26 01:47:21 +0100

    Adds introspection into present chains + refines persistance API.

--- ledger/src/main/kotlin/pt/um/lei/masb/blockchain/service/LedgerHandle.kt
+++ ledger/src/main/kotlin/pt/um/lei/masb/blockchain/service/LedgerHandle.kt
@@ -1,13 +1,17 @@
 package pt.um.lei.masb.blockchain.service
 
+import com.orientechnologies.orient.core.id.ORID
 import com.orientechnologies.orient.core.record.OElement
 import mu.KLogging
 import pt.um.lei.masb.blockchain.data.BlockChainData
-import pt.um.lei.masb.blockchain.ledger.LedgerId
-import pt.um.lei.masb.blockchain.ledger.LedgerParams
-import pt.um.lei.masb.blockchain.persistance.NewInstanceSession
-import pt.um.lei.masb.blockchain.persistance.PersistenceWrapper
+import pt.um.lei.masb.blockchain.ledger.config.LedgerId
+import pt.um.lei.masb.blockchain.ledger.config.LedgerParams
 import pt.um.lei.masb.blockchain.persistance.Storable
+import pt.um.lei.masb.blockchain.persistance.database.NewInstanceSession
+import pt.um.lei.masb.blockchain.persistance.results.QueryResult
+import pt.um.lei.masb.blockchain.persistance.transactions.PersistenceWrapper
+import pt.um.lei.masb.blockchain.results.intoLedger
+import pt.um.lei.masb.blockchain.service.results.LedgerListResult
 import pt.um.lei.masb.blockchain.service.results.LedgerResult
 
 
@@ -19,11 +23,25 @@ class LedgerHandle internal constructor(
     val ledgerId: LedgerId
 ) : Storable, ServiceHandle {
     //TODO: efficiently retrieve chains registered for this ledger.
-    val knownChainTypes: List<Class<BlockChainData>> = emptyList()
+    val knownChainTypes: QueryResult<List<String>>
+        get() = pw.getKnownChainHandleTypes(
+            ledgerId.hashId
+        )
+
+    internal val knownChainIDs: QueryResult<List<ORID>>
+        get() = pw.getKnownChainHandleIDs(
+            ledgerId.hashId
+        )
+
+    val knownChains: LedgerListResult<ChainHandle>
+        get() = pw.getKnownChainHandles(
+            ledgerId.params.crypter.id,
+            ledgerId.hashId
+        )
 
     init {
         pw.registerDefaultClusters(
-            ledgerId.hash
+            ledgerId.hashId
         )
     }
 
@@ -55,12 +73,23 @@ class LedgerHandle internal constructor(
     fun <T : BlockChainData> getChainHandleOf(
         clazz: Class<T>
     ): LedgerResult<ChainHandle> =
-        pw.getChainHandle(clazz, ledgerId)
+        pw.getChainHandle(
+            ledgerId.params.crypter.id,
+            clazz,
+            ledgerId.hashId
+        )
  
     fun <T : BlockChainData> registerNewChainHandleOf(
         clazz: Class<T>
     ): LedgerResult<ChainHandle> =
-        pw.tryAddChainHandle(clazz, ledgerId)
+        ChainHandle(
+            pw, ledgerId.params,
+            clazz.name, ledgerId.hashId
+        ).let {
+            pw.tryAddChainHandle(it).intoLedger {
+                it
+            }
+        }
 
     companion object : KLogging()
 }
