40de5827c67bf8b6395ae9ca14385dbb5634c2fa
André Diogo <redan.blue27@gmail.com>
2019-04-26 01:58:43 +0100

    Adjusts imports.

--- ledger/src/main/kotlin/pt/um/lei/masb/blockchain/service/CategoryTypes.kt
+++ ledger/src/main/kotlin/pt/um/lei/masb/blockchain/service/CategoryTypes.kt
@@ -2,8 +2,8 @@ package pt.um.lei.masb.blockchain.service
 
 import com.orientechnologies.orient.core.record.OElement
 import pt.um.lei.masb.blockchain.ledger.LedgerContract
-import pt.um.lei.masb.blockchain.persistance.NewInstanceSession
 import pt.um.lei.masb.blockchain.persistance.Storable
+import pt.um.lei.masb.blockchain.persistance.database.NewInstanceSession
 
 data class CategoryTypes(
     internal val categoryTypes: List<String>
