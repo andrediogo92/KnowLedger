6b9495a0d78e44d56b766d855d8a01b8f24c6b40
André Diogo <redan.blue27@gmail.com>
2019-05-22 15:02:13 +0100

    Moves ledger contract into common interfaces.

---
+++ common/src/main/kotlin/pt/um/masb/common/storage/LedgerContract.kt
@@ -0,0 +1,11 @@
+package pt.um.masb.common.storage
+
+/**
+ * [LedgerContract] applies to top-level
+ * ledger classes.
+ *
+ * This interface serves as a bound for
+ * main ledger classes, such as blocks and
+ * transactions.
+ */
+interface LedgerContract
