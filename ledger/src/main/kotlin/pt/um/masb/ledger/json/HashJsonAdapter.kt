db0e4b7ae2dcee534f5fd2fab3630d4a5786cbd0
André Diogo <redan.blue27@gmail.com>
2019-05-22 15:27:01 +0100

    Adapts hash json adapter to new class based hash.

--- ledger/src/main/kotlin/pt/um/masb/ledger/json/HashJsonAdapter.kt
+++ ledger/src/main/kotlin/pt/um/masb/ledger/json/HashJsonAdapter.kt
@@ -2,17 +2,16 @@ package pt.um.masb.ledger.json
 
 import com.squareup.moshi.FromJson
 import com.squareup.moshi.ToJson
-import pt.um.masb.common.Hash
-import pt.um.masb.common.print
-import pt.um.masb.common.toHashFromHexString
+import pt.um.masb.common.hash.Hash
+import pt.um.masb.common.misc.hashFromHexString
 
 class HashJsonAdapter {
     @ToJson
     fun hashToJson(hash: Hash): String =
-        hash.print()
+        hash.print
 
     @FromJson
     fun hashFromJson(hash: String): Hash =
-        hash.toHashFromHexString()
+        hash.hashFromHexString
 
 }
