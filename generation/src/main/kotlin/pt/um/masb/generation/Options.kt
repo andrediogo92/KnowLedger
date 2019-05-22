96386607e6a7f0eeee11a797ea9096bfb9e34f43
André Diogo <redan.blue27@gmail.com>
2019-05-22 15:21:53 +0100

    Adds tentative support for auto generating storage adapters.

---
+++ generation/src/main/kotlin/pt/um/masb/generation/Options.kt
@@ -0,0 +1,5 @@
+package pt.um.masb.generation
+
+object Options {
+    const val KAPT_KOTLIN_GENERATED_OPTION_NAME = "kapt.kotlin.generated"
+}
---
+++ generation/src/main/kotlin/pt/um/masb/generation/PersistanceGenerator.kt
@@ -0,0 +1,93 @@
+package pt.um.masb.generation
+
+import com.google.auto.service.AutoService
+import com.squareup.kotlinpoet.FileSpec
+import com.squareup.kotlinpoet.TypeSpec
+import pt.um.masb.annotations.AutoStorable
+import pt.um.masb.generation.Options.KAPT_KOTLIN_GENERATED_OPTION_NAME
+import java.io.File
+import javax.annotation.processing.AbstractProcessor
+import javax.annotation.processing.ProcessingEnvironment
+import javax.annotation.processing.Processor
+import javax.annotation.processing.RoundEnvironment
+import javax.annotation.processing.SupportedSourceVersion
+import javax.lang.model.SourceVersion
+import javax.lang.model.element.Element
+import javax.lang.model.element.ElementKind
+import javax.lang.model.element.Name
+import javax.lang.model.element.PackageElement
+import javax.lang.model.element.TypeElement
+import javax.tools.Diagnostic
+
+@AutoService(Processor::class)
+@SupportedSourceVersion(SourceVersion.RELEASE_8)
+class PersistanceGenerator : AbstractProcessor() {
+    private lateinit var kaptKotlinGenerated: File
+
+    override fun init(processingEnv: ProcessingEnvironment) {
+        super.init(processingEnv)
+        //Trigger NPE if option is broken
+        kaptKotlinGenerated = File(
+            processingEnv.options[KAPT_KOTLIN_GENERATED_OPTION_NAME]!!
+        )
+    }
+
+    override fun getSupportedAnnotationTypes(): Set<String> =
+        setOf("pt.um.lei.masb.agent.annotations.AutoStorable")
+
+    override fun getSupportedOptions(): Set<String> =
+        setOf(KAPT_KOTLIN_GENERATED_OPTION_NAME)
+
+    override fun process(
+        annotations: Set<TypeElement>,
+        roundEnv: RoundEnvironment
+    ): Boolean {
+        val elements = roundEnv.getElementsAnnotatedWith(
+            AutoStorable::class.java
+        )
+        for (element in elements) {
+            if (element.kind != ElementKind.CLASS) {
+                processingEnv.messager.printMessage(
+                    Diagnostic.Kind.ERROR,
+                    "Must be applied only to classes",
+                    element
+                )
+            } else {
+                val typeElement = element.toTypeElementOrNull() ?: continue
+                processSingleClass(
+                    roundEnv,
+                    processingEnv.elementUtils.getPackageOf(typeElement),
+                    typeElement.simpleName,
+                    typeElement.enclosedElements
+                )
+            }
+        }
+        return true
+    }
+
+    private fun processSingleClass(
+        roundEnv: RoundEnvironment,
+        packageElement: PackageElement,
+        simpleName: Name,
+        enclosedElements: List<Element>
+    ) {
+        val implements = TypeSpec.classBuilder("${simpleName}StorageAdapter")//.addSuperinterface()
+
+        FileSpec
+            .builder(packageElement.simpleName.toString(), simpleName.toString())
+            .build().writeTo(kaptKotlinGenerated)
+    }
+
+    private fun Element.toTypeElementOrNull(): TypeElement? {
+        if (this !is TypeElement) {
+            processingEnv.messager.printMessage(
+                Diagnostic.Kind.ERROR,
+                "Invalid element type, class expected",
+                this
+            )
+            return null
+        }
+
+        return this
+    }
+}
