0a1ec92e1554a0265dd4853aa00c4a48fa22e78e
André Diogo <redan.blue27@gmail.com>
2019-05-27 17:19:30 +0100

    Defines data formula via interface + Adds default formula object.

--- common/src/main/kotlin/pt/um/masb/common/data/DataFormula.kt
+++ common/src/main/kotlin/pt/um/masb/common/data/DataFormula.kt
@@ -3,48 +3,15 @@ package pt.um.masb.common.data
 import java.math.BigDecimal
 import java.math.MathContext
 
-typealias DataFormula = (
-    base: Int,
-    timeBase: Int,
-    deltaTime: BigDecimal,
-    valueBase: Int,
-    deltaValue: BigDecimal,
-    constant: Int,
-    threshold: Int,
-    mathContext: MathContext
-) -> Payout
-
-
-fun calculateDiff(
-    base: Int,
-    timeBase: Int,
-    deltaTime: BigDecimal,
-    valueBase: Int,
-    deltaValue: BigDecimal,
-    constant: Int,
-    threshold: Int,
-    mathContext: MathContext
-): Payout {
-    val standardDivisor = BigDecimal(threshold * constant)
-    val timeFactor = deltaTime
-        .multiply(BigDecimal(timeBase))
-        .pow(2, mathContext)
-        .divide(standardDivisor, mathContext)
-
-    val valueFactor = deltaValue
-        .divide(BigDecimal(2), mathContext)
-        .multiply(BigDecimal(valueBase))
-        .divide(standardDivisor, mathContext)
-
-    val baseFactor = BigDecimal(base).divide(standardDivisor, mathContext)
-    return Payout(
-        timeFactor
-            .add(valueFactor)
-            .add(baseFactor)
-    )
-}
-
-object DataDefaults {
-    const val DEFAULT_VALUABLE: Int = 5
-    const val DEFAULT_UNKNOWN: Int = 50
+interface DataFormula {
+    fun calculateDiff(
+        base: Long,
+        timeBase: Long,
+        deltaTime: BigDecimal,
+        valueBase: Long,
+        deltaValue: BigDecimal,
+        constant: Long,
+        threshold: Long,
+        mathContext: MathContext
+    ): Payout
 }
---
+++ common/src/main/kotlin/pt/um/masb/common/data/DefaultDiff.kt
@@ -0,0 +1,45 @@
+package pt.um.masb.common.data
+
+import java.math.BigDecimal
+import java.math.MathContext
+
+object DefaultDiff : DataFormula {
+    override fun calculateDiff(
+        base: Long,
+        timeBase: Long,
+        deltaTime: BigDecimal,
+        valueBase: Long,
+        deltaValue: BigDecimal,
+        constant: Long,
+        threshold: Long,
+        mathContext: MathContext
+    ): Payout {
+        val standardDivisor = BigDecimal(threshold * constant)
+        val timeFactor = deltaTime
+            .multiply(BigDecimal(timeBase))
+            .pow(2, mathContext)
+            .divide(standardDivisor, mathContext)
+
+        val valueFactor = deltaValue
+            .divide(BigDecimal(2), mathContext)
+            .multiply(BigDecimal(valueBase))
+            .divide(standardDivisor, mathContext)
+
+        val baseFactor = BigDecimal(base).divide(standardDivisor, mathContext)
+        return Payout(
+            timeFactor
+                .add(valueFactor)
+                .add(baseFactor)
+        )
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is DefaultDiff) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return javaClass.hashCode()
+    }
+}
