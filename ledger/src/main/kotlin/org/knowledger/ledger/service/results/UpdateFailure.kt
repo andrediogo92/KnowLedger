6de20ddf2c127eed6d0f30d31435cbdcbe13e37e
André Diogo <redan.blue27@gmail.com>
2019-07-18 20:58:55 +0100

    Adds storage aware specific update failure.

---
+++ ledger/src/main/kotlin/org/knowledger/ledger/service/results/UpdateFailure.kt
@@ -0,0 +1,26 @@
+package org.knowledger.ledger.service.results
+
+import org.knowledger.common.results.Failable
+import org.knowledger.common.results.HardFailure
+import org.knowledger.common.results.PropagatedFailure
+
+sealed class UpdateFailure : Failable {
+    object NotYetStored : UpdateFailure() {
+        override val cause: String =
+            "Element has not yet been persisted first."
+    }
+
+    data class FailedToSave(
+        override val cause: String
+    ) : UpdateFailure()
+
+    data class UnknownFailure(
+        override val cause: String,
+        override val exception: Exception? = null
+    ) : UpdateFailure(), HardFailure
+
+    data class Propagated(
+        override val pointOfFailure: String,
+        override val failable: Failable
+    ) : UpdateFailure(), PropagatedFailure
+}
