8c527b5df0d1677fac954812a6a834f9e94b004a
André Diogo <redan.blue27@gmail.com>
2019-07-18 21:50:04 +0100

    Adds polymorphic json adapters for new interface types.

--- ledger/src/main/kotlin/org/knowledger/ledger/json/Moshi.kt
+++ ledger/src/main/kotlin/org/knowledger/ledger/json/Moshi.kt
@@ -4,15 +4,24 @@ import com.squareup.moshi.Moshi
 import com.squareup.moshi.adapters.PolymorphicJsonAdapterFactory
 import org.knowledger.common.data.LedgerData
 import org.knowledger.ledger.config.ChainId
-import org.knowledger.ledger.config.StorageAwareChainId
-import org.knowledger.ledger.config.StorageUnawareChainId
+import org.knowledger.ledger.config.chainid.StorageAwareChainId
+import org.knowledger.ledger.config.chainid.StorageUnawareChainId
 import org.knowledger.ledger.data.DummyData
 import org.knowledger.ledger.service.pool.StorageAwareTransactionPool
 import org.knowledger.ledger.service.pool.StorageUnawareTransactionPool
 import org.knowledger.ledger.service.pool.TransactionPool
 import org.knowledger.ledger.storage.Block
-import org.knowledger.ledger.storage.StorageAwareBlock
-import org.knowledger.ledger.storage.StorageUnawareBlock
+import org.knowledger.ledger.storage.BlockHeader
+import org.knowledger.ledger.storage.Coinbase
+import org.knowledger.ledger.storage.MerkleTree
+import org.knowledger.ledger.storage.block.StorageAwareBlock
+import org.knowledger.ledger.storage.block.StorageUnawareBlock
+import org.knowledger.ledger.storage.blockheader.StorageAwareBlockHeader
+import org.knowledger.ledger.storage.blockheader.StorageUnawareBlockHeader
+import org.knowledger.ledger.storage.coinbase.StorageAwareCoinbase
+import org.knowledger.ledger.storage.coinbase.StorageUnawareCoinbase
+import org.knowledger.ledger.storage.merkletree.StorageAwareMerkleTree
+import org.knowledger.ledger.storage.merkletree.StorageUnawareMerkleTree
 
 
 fun Moshi.Builder.addLedgerAdapters(
@@ -33,19 +42,37 @@ fun Moshi.Builder.addLedgerAdapters(
         .add(
             PolymorphicJsonAdapterFactory
                 .of(Block::class.java, "type")
-                .withSubtype(StorageAwareBlock::class.java, "storageAware")
-                .withSubtype(StorageUnawareBlock::class.java, "storageUnaware")
+                .withSubtype(StorageAwareBlock::class.java, "StorageAware")
+                .withSubtype(StorageUnawareBlock::class.java, "StorageUnaware")
+        )
+        .add(
+            PolymorphicJsonAdapterFactory
+                .of(BlockHeader::class.java, "type")
+                .withSubtype(StorageAwareBlockHeader::class.java, "StorageAware")
+                .withSubtype(StorageUnawareBlockHeader::class.java, "StorageUnaware")
+        )
+        .add(
+            PolymorphicJsonAdapterFactory
+                .of(Coinbase::class.java, "type")
+                .withSubtype(StorageAwareCoinbase::class.java, "StorageAware")
+                .withSubtype(StorageUnawareCoinbase::class.java, "StorageUnaware")
+        )
+        .add(
+            PolymorphicJsonAdapterFactory
+                .of(MerkleTree::class.java, "type")
+                .withSubtype(StorageAwareMerkleTree::class.java, "StorageAware")
+                .withSubtype(StorageUnawareMerkleTree::class.java, "StorageUnaware")
         )
         .add(
             PolymorphicJsonAdapterFactory
                 .of(ChainId::class.java, "type")
-                .withSubtype(StorageAwareChainId::class.java, "storageAware")
-                .withSubtype(StorageUnawareChainId::class.java, "storageUnaware")
+                .withSubtype(StorageAwareChainId::class.java, "StorageAware")
+                .withSubtype(StorageUnawareChainId::class.java, "StorageUnaware")
         ).add(
             PolymorphicJsonAdapterFactory
                 .of(TransactionPool::class.java, "type")
-                .withSubtype(StorageAwareTransactionPool::class.java, "storageAware")
-                .withSubtype(StorageUnawareTransactionPool::class.java, "storageUnaware")
+                .withSubtype(StorageAwareTransactionPool::class.java, "StorageAware")
+                .withSubtype(StorageUnawareTransactionPool::class.java, "StorageUnaware")
         )
         .add(SortedSetJsonAdapterFactory)
 
