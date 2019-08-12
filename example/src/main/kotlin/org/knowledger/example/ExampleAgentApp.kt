0716edea83cb3f23cc486c67903bc5c936ce6466
André Diogo <redan.blue27@gmail.com>
2019-08-12 01:06:03 +0100

    Extracts a simple working example of running agents.

--- example/src/main/kotlin/org/knowledger/example/ExampleAgentApp.kt
+++ example/src/main/kotlin/org/knowledger/example/ExampleAgentApp.kt
@@ -1,2 +1,34 @@
 package org.knowledger.example
 
+import org.knowledger.agent.AgentContainer
+import org.knowledger.ledger.core.results.unwrap
+import org.knowledger.ledger.service.handles.LedgerHandle
+
+class ExampleAgentApp {
+
+    companion object {
+        @JvmStatic
+        fun main() {
+
+            val container = AgentContainer(
+                host = "localhost", port = 9888,
+                containerName = "LedgerMain", isMainContainer = true,
+                hasGUI = true
+            )
+            val handle =
+                LedgerHandle.Builder()
+                    .withLedgerIdentity("test")
+                    .unwrap()
+                    .build()
+                    .unwrap()
+
+            container.runLedgerAgent(
+                "MinerAgent",
+                "pt.um.lei.masb.agent.SingleChainAgent",
+                handle,
+                emptyArray()
+            )
+
+        }
+    }
+}
