2d9a2691aee328895e1ab704b2e64118b1339c3a
André Diogo <redan.blue27@gmail.com>
2019-08-12 01:03:17 +0100

    Reworks agent container to expose a simple agent running API.

---
+++ agent/src/main/kotlin/org/knowledger/agent/AgentContainer.kt
@@ -0,0 +1,73 @@
+package org.knowledger.agent
+
+import jade.core.Profile
+import jade.core.ProfileImpl
+import jade.core.Runtime
+import jade.wrapper.AgentController
+import jade.wrapper.ContainerController
+import org.knowledger.ledger.service.handles.LedgerHandle
+
+/**
+ * Provides thread-safe access to an agent container that
+ * can run intelligent agents.
+ */
+class AgentContainer(
+    host: String, port: Int,
+    containerName: String,
+    isMainContainer: Boolean,
+    hasGUI: Boolean
+) {
+    // Get the JADE runtime interface (singleton)
+    private val rt: Runtime = Runtime.instance()
+    // Create a Profile, where the launch arguments are stored
+    private val profile: Profile = ProfileImpl(
+        host, port,
+        containerName, isMainContainer
+    ).also {
+        it.setParameter(Profile.GUI, hasGUI.toString())
+    }
+
+    val container: ContainerController =
+        rt.createAgentContainer(profile)
+
+    private val iAgents =
+        mutableListOf<AgentController>()
+
+    val agents: List<AgentController>
+        get() = iAgents
+
+
+    fun runAgent(
+        name: String,
+        classpath: String,
+        arguments: Array<Any>
+    ) {
+        val agent = container.createNewAgent(
+            name, classpath, arguments
+        )
+        synchronized(iAgents) {
+            iAgents += agent
+        }
+        agent.start()
+    }
+
+    fun runLedgerAgent(
+        name: String,
+        classpath: String,
+        handle: LedgerHandle,
+        arguments: Array<Any>
+    ) {
+        runAgent(name, classpath, arrayOf(handle, *arguments))
+    }
+
+    fun killAgent(agent: String) {
+        val index =
+            agents.indexOfFirst {
+                it.name == agent
+            }
+        agents[index].kill()
+        synchronized(iAgents) {
+            iAgents.removeAt(index)
+        }
+    }
+}
