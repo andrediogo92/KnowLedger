42bc545c82ddda00784e8b32eb45c3b5a234bc0b
André Diogo <redan.blue27@gmail.com>
2019-08-19 17:09:35 +0100

    Adds mapping extensions to pair types.

---
+++ ledger-core/src/main/kotlin/org/knowledger/ledger/core/misc/PairExtensions.kt
@@ -0,0 +1,17 @@
+package org.knowledger.ledger.core.misc
+
+inline fun <T, U, V> Pair<T, U>.mapSecond(
+    map: (U) -> V
+): Pair<T, V> =
+    Pair(first, map(second))
+
+inline fun <T, U, V> Pair<T, U>.mapFirst(
+    map: (T) -> V
+): Pair<V, U> =
+    Pair(map(first), second)
+
+inline fun <T, U, V, W> Pair<T, U>.map(
+    left: (T) -> V,
+    right: (U) -> W
+): Pair<V, W> =
+    Pair(left(first), right(second))
