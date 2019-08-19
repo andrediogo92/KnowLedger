fac5db865bd86220f24dc12e07a22dedcccd6163
André Diogo <redan.blue27@gmail.com>
2019-08-19 17:10:43 +0100

    Introduces Tag as Hash alias. Represents hashed type data.

---
+++ ledger-core/src/main/kotlin/org/knowledger/ledger/core/data/Tag.kt
@@ -0,0 +1,5 @@
+package org.knowledger.ledger.core.data
+
+import org.knowledger.ledger.core.hash.Hash
+
+typealias Tag = Hash
--- ledger-core/src/main/kotlin/org/knowledger/ledger/core/misc/StringExtensions.kt
+++ ledger-core/src/main/kotlin/org/knowledger/ledger/core/misc/StringExtensions.kt
@@ -1,5 +1,6 @@
 package org.knowledger.ledger.core.misc
 
+import org.knowledger.ledger.core.data.Tag
 import org.knowledger.ledger.core.hash.AvailableHashAlgorithms
 
 @Suppress("NOTHING_TO_INLINE")
@@ -12,7 +13,7 @@ inline fun String.encodeStringToUTF8(): ByteArray =
 inline fun ByteArray.decodeUTF8ToString(): String =
     this.decodeToString()
 
-val <T> Class<T>.classDigest: String
+val <T> Class<T>.classDigest: Tag
     get() = AvailableHashAlgorithms.SHA3256Hasher.applyHash(
         toGenericString()
-    ).base64Encode()
+    )
