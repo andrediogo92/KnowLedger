5960b500f98fcb093056faf0fb244d68089b4063
André Diogo <redan.blue27@gmail.com>
2019-08-30 19:43:27 +0100

    Adds filtering by index.

--- ledger-core/src/main/kotlin/org/knowledger/ledger/core/misc/ArrayExtensions.kt
+++ ledger-core/src/main/kotlin/org/knowledger/ledger/core/misc/ArrayExtensions.kt
@@ -12,7 +12,45 @@ inline fun <T, reified R> Array<out T>.mapAndAdd(
 ): Array<R> =
     Array(size + toAdd.size) {
         when (it) {
-            in 0 until toAdd.size -> transform(toAdd[it])
+            in toAdd.indices -> transform(toAdd[it])
             else -> transform(this[it - toAdd.size])
         }
     }
+
+fun <T> List<T>.filterByIndex(
+    function: (Int) -> Boolean
+): List<T> {
+    val mutList = mutableListOf<T>()
+    var i = 0
+    while (i < size) {
+        if (function(i)) {
+            mutList += this[i]
+        }
+        i++
+    }
+    return mutList
+}
+
+fun <T> Iterable<T>.filterByIndex(
+    function: (Int) -> Boolean
+): Iterable<T> =
+    iterator().filterByIndex(function)
+
+fun <T> Iterator<T>.filterByIndex(
+    function: (Int) -> Boolean
+): List<T> {
+    val mutList = mutableListOf<T>()
+    var i = 0
+    while (hasNext()) {
+        if (function(i)) {
+            mutList += next()
+        }
+        i++
+    }
+    return mutList
+}
+
+fun <T> Sequence<T>.filterByIndex(
+    function: (Int) -> Boolean
+): List<T> =
+    iterator().filterByIndex(function)
