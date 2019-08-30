3bead0b3b931f08475ae6e4ab5db31bbf3997c60
André Diogo <redan.blue27@gmail.com>
2019-08-30 20:10:16 +0100

    Makes sizeable abstract.
    No longer uses jol heap introspection in favor of serialized size.

--- ledger-core/src/main/kotlin/org/knowledger/ledger/core/Sizeable.kt
+++ ledger-core/src/main/kotlin/org/knowledger/ledger/core/Sizeable.kt
@@ -1,14 +1,14 @@
 package org.knowledger.ledger.core
 
-import org.openjdk.jol.info.GraphLayout
-
 /**
  * Report an approximate size in bytes
  * of the underlying object.
  */
 interface Sizeable {
+    /**
+     * Calculates the approximate size of the transaction.
+     *
+     * @property approximateSize The size of the transaction in bytes.
+     */
     val approximateSize: Long
-        get() = GraphLayout
-            .parseInstance(this)
-            .totalSize()
 }
