569e4a8a5fc50ca8c2318757be7a0def067199d1
André Diogo <redan.blue27@gmail.com>
2019-08-30 19:28:28 +0100

    Adds new ledger modules dependencies + serialization.
    Removes kotlin reflection and openjdk jol dependency.

--- ledger/build.gradle.kts
+++ ledger/build.gradle.kts
@@ -1,36 +1,23 @@
-import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 version = "0.1"
 
 plugins {
     kotlin("jvm")
-    kotlin("kapt")
+    id("org.jetbrains.kotlin.plugin.serialization")
     id("org.jetbrains.kotlin.plugin.noarg")
     id("org.jetbrains.dokka")
-    //    id("kotlinx-serialization")
 }
 
 dependencies {
     //Project dependencies
-    compileOnly(project(":annotations"))
     implementation(project(":ledger-core"))
-
-    //Annotation Processing
-    kapt(Libs.moshiCodeGen)
-
+    implementation(project(":ledger-orient"))
+    implementation(project(":ledger-crypto"))
 
     //Regular dependencies
     implementation(kotlin("stdlib", Versions.kotlin))
-    implementation(kotlin("reflect", Versions.kotlin))
-    //implementation(Libs.arrowK)
-    implementation(Libs.bouncyCastle)
-    //implementation(Libs.coroutines)
-    implementation(Libs.jol)
-    Libs.moshi.forEach {
-        implementation(it)
-    }
-    //implementation(Libs.serialization)
+    implementation(Libs.serialization)
     Libs.tinylog.forEach {
         implementation(it)
     }
@@ -89,12 +76,6 @@ tasks {
     }
 
     withType<KotlinCompile> {
-        kotlinOptions.freeCompilerArgs += "-XXLanguage:+InlineClasses"
         kotlinOptions.jvmTarget = "1.8"
     }
-}
-
-
-sourceSets["main"].withConvention(KotlinSourceSet::class) {
-    kotlin.srcDir("${buildDir.absolutePath}/generated/source/kaptKotlin/")
 }
