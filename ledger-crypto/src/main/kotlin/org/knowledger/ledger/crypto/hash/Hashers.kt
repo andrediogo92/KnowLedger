a3d51757c1200624fd19d9280e32f69840c36aa3
André Diogo <redan.blue27@gmail.com>
2019-09-02 15:13:29 +0100

    Makes hashers serializable. This makes it possible to hash the CBOR representation.
    Adds extra exception info.

--- ledger-crypto/src/main/kotlin/org/knowledger/ledger/crypto/hash/Hashers.kt
+++ ledger-crypto/src/main/kotlin/org/knowledger/ledger/crypto/hash/Hashers.kt
@@ -1,20 +1,26 @@
 package org.knowledger.ledger.crypto.hash
 
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.cbor.Cbor
 import org.bouncycastle.jce.provider.BouncyCastleProvider
 import org.knowledger.ledger.core.hash.Hash
 import org.knowledger.ledger.core.hash.Hasher
-import org.knowledger.ledger.core.misc.asBytes
-import org.knowledger.ledger.core.misc.flattenBytes
+import org.knowledger.ledger.crypto.serial.HashAlgorithmSerializer
 import java.security.MessageDigest
 import java.security.Security
 
-
+@Serializable(with = HashAlgorithmSerializer::class)
 sealed class AvailableHashAlgorithms(algorithmTag: String) : Hasher {
     val digester: MessageDigest by lazy {
         MessageDigest.getInstance(
             algorithmTag
         )
     }
+
+    override val id: Hash by lazy {
+        Hash(Cbor.plain.dump(serializer(), this))
+    }
+
 
     override fun applyHash(input: ByteArray): Hash =
         Hash(digester.digest(input))
@@ -24,20 +30,14 @@ sealed class AvailableHashAlgorithms(algorithmTag: String) : Hasher {
         digester.digestLength
     }
 
-    override val id: Hash by lazy {
-        val provider = digester.provider
+    private fun checkAlgorithm(
+        digestLength: Int, algorithm: String,
+        providerName: String, providerVersion: Double
+    ): Boolean = digestLength == digester.digestLength &&
+            algorithm == digester.algorithm &&
+            providerName == digester.provider.name &&
+            providerVersion == digester.provider.version
 
-        Hash(
-            digester.digest(
-                flattenBytes(
-                    digester.digestLength.asBytes,
-                    digester.algorithm.toByteArray(),
-                    provider.name.toByteArray(),
-                    provider.version.asBytes
-                )
-            )
-        )
-    }
 
     object SHA3256Hasher : AvailableHashAlgorithms("SHA3-256")
     object SHA3512Hasher : AvailableHashAlgorithms("SHA3-512")
@@ -51,8 +51,6 @@ sealed class AvailableHashAlgorithms(algorithmTag: String) : Hasher {
 
     companion object {
         val DEFAULT_HASHER = Blake2b256Hasher
-
-        class NoSuchHasherRegistered : Exception()
 
         init {
             //Ensure Bouncy Castle Crypto provider is present
@@ -74,8 +72,48 @@ sealed class AvailableHashAlgorithms(algorithmTag: String) : Hasher {
                 SHA512Hasher.checkForCrypter(hash) -> SHA512Hasher
                 Keccak256Hasher.checkForCrypter(hash) -> Keccak256Hasher
                 Keccak512Hasher.checkForCrypter(hash) -> Keccak512Hasher
-                else -> throw NoSuchHasherRegistered()
+                else -> throw NoSuchHasherRegistered(hash)
+            }
+
+        fun checkAlgorithms(
+            digestLength: Int, algorithm: String,
+            providerName: String, providerVersion: Double
+        ): AvailableHashAlgorithms? =
+            arrayOf(
+                SHA256Hasher, SHA512Hasher,
+                SHA3256Hasher, SHA3512Hasher,
+                Blake2b256Hasher, Blake2b512Hasher,
+                Blake2s256Hasher, Keccak256Hasher,
+                Keccak512Hasher
+            ).find {
+                it.checkAlgorithm(
+                    digestLength, algorithm,
+                    providerName, providerVersion
+                )
             }
     }
 }
 
+class NoSuchHasherRegistered() : Exception() {
+    override var message: String? = super.message
+
+    constructor(
+        digestLength: Int,
+        algorithm: String,
+        providerName: String,
+        providerVersion: Double
+    ) : this() {
+        message = """No hasher with: 
+            |   Digest length -> $digestLength
+            |   Algorithm -> $algorithm
+            |   Provider -> $providerName
+            |   Provider Version -> $providerVersion
+            """.trimMargin()
+    }
+
+    constructor(
+        hash: Hash
+    ) : this() {
+        message = "No hasher with hash: $hash"
+    }
+}
---
+++ ledger-crypto/src/main/kotlin/org/knowledger/ledger/crypto/serial/HashAlgorithmSerializer.kt
@@ -0,0 +1,58 @@
+package org.knowledger.ledger.crypto.serial
+
+import kotlinx.serialization.CompositeDecoder.Companion.READ_DONE
+import kotlinx.serialization.Decoder
+import kotlinx.serialization.Encoder
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.SerialDescriptor
+import kotlinx.serialization.Serializer
+import kotlinx.serialization.internal.SerialClassDescImpl
+import org.knowledger.ledger.crypto.hash.AvailableHashAlgorithms
+import org.knowledger.ledger.crypto.hash.NoSuchHasherRegistered
+import kotlin.properties.Delegates
+
+@Serializer(forClass = AvailableHashAlgorithms::class)
+object HashAlgorithmSerializer : KSerializer<AvailableHashAlgorithms> {
+    override val descriptor: SerialDescriptor =
+        object : SerialClassDescImpl("HashAlgorithm") {
+            init {
+                addElement("digestLength")
+                addElement("algorithm")
+                addElement("providerName")
+                addElement("providerVersion")
+            }
+        }
+
+    override fun deserialize(decoder: Decoder): AvailableHashAlgorithms {
+        val dec = decoder.beginStructure(descriptor)
+        var digestLength by Delegates.notNull<Int>()
+        lateinit var algorithm: String
+        lateinit var providerName: String
+        var providerVersion by Delegates.notNull<Double>()
+        loop@ while (true) {
+            when (val i = dec.decodeElementIndex(descriptor)) {
+                READ_DONE -> break@loop
+                0 -> digestLength = dec.decodeIntElement(descriptor, i)
+                1 -> algorithm = dec.decodeStringElement(descriptor, i)
+                2 -> providerName = dec.decodeStringElement(descriptor, i)
+                3 -> providerVersion = dec.decodeDoubleElement(descriptor, i)
+            }
+        }
+        return AvailableHashAlgorithms.checkAlgorithms(
+            digestLength, algorithm,
+            providerName, providerVersion
+        ) ?: throw NoSuchHasherRegistered(
+            digestLength, algorithm,
+            providerName, providerVersion
+        )
+    }
+
+    override fun serialize(encoder: Encoder, obj: AvailableHashAlgorithms) {
+        val enc = encoder.beginStructure(descriptor)
+        enc.encodeIntElement(descriptor, 0, obj.digester.digestLength)
+        enc.encodeStringElement(descriptor, 1, obj.digester.algorithm)
+        enc.encodeStringElement(descriptor, 2, obj.digester.provider.name)
+        enc.encodeDoubleElement(descriptor, 3, obj.digester.provider.version)
+        enc.endStructure(descriptor)
+    }
+}
