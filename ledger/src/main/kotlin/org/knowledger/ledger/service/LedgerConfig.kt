8ab662c97de95b97a1d3f8fab182c7b4e1cc0d4f
André Diogo <redan.blue27@gmail.com>
2019-09-03 23:36:18 +0100

    Adds serialization.

--- ledger/src/main/kotlin/org/knowledger/ledger/service/LedgerConfig.kt
+++ ledger/src/main/kotlin/org/knowledger/ledger/service/LedgerConfig.kt
@@ -1,5 +1,7 @@
 package org.knowledger.ledger.service
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 import org.knowledger.ledger.config.CoinbaseParams
 import org.knowledger.ledger.config.LedgerId
 import org.knowledger.ledger.config.LedgerParams
@@ -7,6 +9,8 @@ import org.knowledger.ledger.config.LedgerParams
 /**
  *
  */
+@Serializable
+@SerialName("LedgerConfig")
 data class LedgerConfig(
     val ledgerId: LedgerId,
     val ledgerParams: LedgerParams,
