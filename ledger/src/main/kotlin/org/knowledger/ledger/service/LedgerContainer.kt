1bcec8b46b7d3aab266fac62ff5d95af0cd02e53
André Diogo <redan.blue27@gmail.com>
2019-09-03 22:54:44 +0100

    Adds configured cbor and serial module instances.
    Restricts hasher instance to Hashers.

--- ledger/src/main/kotlin/org/knowledger/ledger/service/LedgerContainer.kt
+++ ledger/src/main/kotlin/org/knowledger/ledger/service/LedgerContainer.kt
@@ -1,17 +1,21 @@
 package org.knowledger.ledger.service
 
+import kotlinx.serialization.cbor.Cbor
+import kotlinx.serialization.modules.SerialModule
 import org.knowledger.ledger.config.CoinbaseParams
 import org.knowledger.ledger.config.LedgerParams
 import org.knowledger.ledger.core.data.DataFormula
 import org.knowledger.ledger.core.hash.Hash
-import org.knowledger.ledger.core.hash.Hasher
+import org.knowledger.ledger.crypto.hash.Hashers
 import org.knowledger.ledger.service.transactions.PersistenceWrapper
 
 internal data class LedgerContainer(
     val ledgerHash: Hash,
-    val hasher: Hasher,
+    val hasher: Hashers,
     val ledgerParams: LedgerParams,
     val coinbaseParams: CoinbaseParams,
+    val serialModule: SerialModule,
     val persistenceWrapper: PersistenceWrapper,
-    val formula: DataFormula
+    val formula: DataFormula,
+    val cbor: Cbor
 )
