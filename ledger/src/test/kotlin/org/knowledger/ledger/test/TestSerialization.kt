13f78cf214815520bb0fa4eff99dbbaa751c0cc3
André Diogo <redan.blue27@gmail.com>
2019-09-04 00:27:32 +0100

    Serialization is tested via json encoder.

--- ledger/src/test/kotlin/org/knowledger/ledger/test/TestSerialization.kt
+++ ledger/src/test/kotlin/org/knowledger/ledger/test/TestSerialization.kt
@@ -1,16 +1,18 @@
 package org.knowledger.ledger.test
 
-import assertk.assertAll
 import assertk.assertThat
 import assertk.assertions.containsExactly
 import assertk.assertions.isEqualTo
 import assertk.assertions.isNotNull
 import assertk.assertions.isTrue
+import kotlinx.serialization.PolymorphicSerializer
+import kotlinx.serialization.UnstableDefault
 import org.junit.jupiter.api.Test
-import org.knowledger.ledger.service.Identity
-import org.knowledger.ledger.storage.Block
+import org.knowledger.ledger.crypto.service.Identity
+import org.knowledger.ledger.storage.block.Block
 import org.tinylog.kotlin.Logger
 
+@UnstableDefault
 class TestSerialization {
     private val id = arrayOf(
         Identity("test1"),
@@ -20,12 +22,12 @@ class TestSerialization {
     val chainId = generateChainId()
 
     val testTransactions =
-        generateXTransactionsWithChain(chainId, id, 10).toSortedSet()
+        generateXTransactions(id, 10).toSortedSet()
 
     @Test
     fun `serialization and deserialization of blocks`() {
         val block = generateBlockWithChain(
-            chainId, id
+            chainId
         )
 
         testTransactions.forEach { t ->
@@ -38,21 +40,18 @@ class TestSerialization {
 
         assertThat(block.data.size).isEqualTo(testTransactions.size)
 
-        val json = moshi.adapter(Block::class.java)
-        val resultingBlock = json.toJson(block)
+        val resultingBlock = json.stringify(PolymorphicSerializer(Block::class), block)
         Logger.debug {
             resultingBlock
         }
-        val rebuiltBlock = json.fromJson(resultingBlock)!!
+        val rebuiltBlock = json.parse(PolymorphicSerializer(Block::class), resultingBlock) as Block
         //Even though everything seems absolutely fine
         //this blows up.
         //Deserialization is unnecessary though.
-        assertAll {
-            assertThat(rebuiltBlock.coinbase).isEqualTo(block.coinbase)
-            assertThat(rebuiltBlock.data.toTypedArray()).containsExactly(*block.data.toTypedArray())
-            assertThat(rebuiltBlock.header).isEqualTo(block.header)
-            assertThat(rebuiltBlock.merkleTree).isEqualTo(block.merkleTree)
-            assertThat(rebuiltBlock).isEqualTo(block)
-        }
+        assertThat(rebuiltBlock.coinbase).isEqualTo(block.coinbase)
+        assertThat(rebuiltBlock.data.toTypedArray()).containsExactly(*block.data.toTypedArray())
+        assertThat(rebuiltBlock.header).isEqualTo(block.header)
+        assertThat(rebuiltBlock.merkleTree).isEqualTo(block.merkleTree)
+        assertThat(rebuiltBlock).isEqualTo(block)
     }
 }
