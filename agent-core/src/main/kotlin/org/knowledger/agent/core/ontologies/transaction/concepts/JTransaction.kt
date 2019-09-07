7dac806c81e0aed3a6b89d32791795dc0feef2dc
André Diogo <redan.blue27@gmail.com>
2019-09-07 22:13:34 +0100

    Makes chain ids nullable for transactions.
    Transactions may need to be broadcast with chain id.

--- agent-core/src/main/kotlin/org/knowledger/agent/core/ontologies/transaction/concepts/JTransaction.kt
+++ agent-core/src/main/kotlin/org/knowledger/agent/core/ontologies/transaction/concepts/JTransaction.kt
@@ -8,7 +8,7 @@ import org.knowledger.agent.core.ontologies.ledger.concepts.JChainId
  * JTransaction in JADE ontology bean form.
  */
 data class JTransaction(
-    var ledgerId: JChainId,
+    var ledgerId: JChainId?,
     var transactionId: String,
     var publicKey: String,
     var data: JPhysicalData,
