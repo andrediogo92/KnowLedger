ec9575b23c9ed410686a7c5d755b9a0e04cdb15c
André Diogo <redan.blue27@gmail.com>
2019-09-10 09:30:24 +0100

    Adds blockheight in blocks request.

--- agent-core/src/main/kotlin/org/knowledger/agent/core/ontologies/block/actions/RequestBlocksFrom.kt
+++ agent-core/src/main/kotlin/org/knowledger/agent/core/ontologies/block/actions/RequestBlocksFrom.kt
@@ -3,4 +3,6 @@ package org.knowledger.agent.core.ontologies.block.actions
 import jade.content.AgentAction
 
 
-class RequestBlocksFrom : AgentAction
+data class RequestBlocksFrom(
+    val from: Long
+) : AgentAction
