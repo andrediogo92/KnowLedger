c6ecb8a081480640bcf24635c03952e67ec4ca77
André Diogo <redan.blue27@gmail.com>
2019-10-01 20:15:54 +0100

    Uses hash serializer for encoding and decoding.

--- ledger-crypto/src/main/kotlin/org/knowledger/ledger/crypto/serial/DefaultDataFormulaSerializer.kt
+++ ledger-crypto/src/main/kotlin/org/knowledger/ledger/crypto/serial/DefaultDataFormulaSerializer.kt
@@ -7,8 +7,8 @@ import kotlinx.serialization.SerialDescriptor
 import kotlinx.serialization.Serializer
 import kotlinx.serialization.internal.SerialClassDescImpl
 import org.knowledger.ledger.core.data.DefaultDiff
+import org.knowledger.ledger.core.hash.Hash
 import org.knowledger.ledger.core.misc.classDigest
-import org.knowledger.ledger.core.misc.hashFromHexString
 import org.knowledger.ledger.crypto.hash.Hashers
 
 @Serializer(forClass = DefaultDiff::class)
@@ -22,20 +22,19 @@ object DefaultDataFormulaSerializer : KSerializer<DefaultDiff> {
 
     override fun deserialize(decoder: Decoder): DefaultDiff {
         val dec = decoder.beginStructure(descriptor)
-        val hash = dec.decodeStringElement(descriptor, 0)
+        val hash = dec.decodeSerializableElement(descriptor, 0, Hash.serializer())
         dec.endStructure(descriptor)
         assert(
-            hash.hashFromHexString() ==
-                    DefaultDiff.classDigest(Hashers.SHA3512Hasher)
+            hash == DefaultDiff.classDigest(Hashers.SHA3512Hasher)
         )
         return DefaultDiff
     }
 
     override fun serialize(encoder: Encoder, obj: DefaultDiff) {
         val enc = encoder.beginStructure(descriptor)
-        enc.encodeStringElement(
-            descriptor, 0,
-            DefaultDiff.classDigest(Hashers.SHA3512Hasher).print
+        enc.encodeSerializableElement(
+            descriptor, 0, Hash.serializer(),
+            DefaultDiff.classDigest(Hashers.SHA3512Hasher)
         )
         enc.endStructure(descriptor)
     }
