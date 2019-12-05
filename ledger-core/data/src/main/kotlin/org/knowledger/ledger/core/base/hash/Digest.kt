2c0c6dc41425f33d09a0470fc039586c22c12a20
André Diogo <redan.blue27@gmail.com>
2019-12-05 16:21:30 +0000

    Uses javaClass directly to apply classDigest.

--- ledger-core/data/src/main/kotlin/org/knowledger/ledger/core/base/hash/Digest.kt
+++ ledger-core/data/src/main/kotlin/org/knowledger/ledger/core/base/hash/Digest.kt
@@ -3,9 +3,7 @@ package org.knowledger.ledger.core.base.hash
 import org.knowledger.ledger.core.base.data.Tag
 
 fun <T : Any> T.classDigest(hasher: Hasher): Tag =
-    hasher.applyHash(
-        this::class.java.toGenericString()
-    )
+    this.javaClass.classDigest(hasher)
 
 fun <T> Class<T>.classDigest(hasher: Hasher): Tag =
     hasher.applyHash(
