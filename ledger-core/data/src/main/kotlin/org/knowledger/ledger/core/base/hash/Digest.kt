181d31fef6abbe7a3993aac020f38b6e82ba25e3
André Diogo <redan.blue27@gmail.com>
2020-01-08 15:27:17 +0000

    Revamps generic classDigest implementation via generic reflection algorithm.
    This should be painfully painfully slow. Must be circumvented through code generation or manual implementation.

--- ledger-core/data/src/main/kotlin/org/knowledger/ledger/core/base/hash/Digest.kt
+++ ledger-core/data/src/main/kotlin/org/knowledger/ledger/core/base/hash/Digest.kt
@@ -3,9 +3,33 @@ package org.knowledger.ledger.core.base.hash
 import org.knowledger.ledger.core.base.data.Tag
 
 fun <T : Any> T.classDigest(hasher: Hasher): Tag =
-    this.javaClass.classDigest(hasher)
+    javaClass.classDigest(hasher)
 
-fun <T> Class<T>.classDigest(hasher: Hasher): Tag =
-    hasher.applyHash(
-        toGenericString()
-    )
+
+fun <T : Any> Class<in T>.classDigest(hasher: Hasher): Tag {
+    var clazz: Class<*>? = this
+    while (
+        clazz != null &&
+        clazz.isInterface &&
+        (!clazz.isMemberClass || !clazz.isLocalClass || !clazz.isAnonymousClass || !clazz.isPrimitive)
+    ) {
+        clazz = clazz.declaringClass
+    }
+    return if (clazz != null) {
+        hasher.applyHash(
+            if (clazz.isPrimitive) {
+                clazz.canonicalName
+            } else {
+                clazz.declaredFields.joinToString {
+                    "${it.name}${it.type.classDigest(hasher)}"
+                }
+            })
+    } else {
+        throw ClassNotFoundException(
+            "Couldn't find a base concrete class for ${javaClass.toGenericString()}: $this"
+        )
+    }
+}
+
+inline fun <reified T : Any> classDigest(hasher: Hasher): Tag =
+    T::class.java.classDigest(hasher)
