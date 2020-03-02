f455e233754f63c3381bdbc4cf31c75042296c96
André Diogo <redan.blue27@gmail.com>
2020-03-02 22:12:31 +0000

    Makes byte encodings comparable (similar to string comparisons).

--- ledger-core/data/src/main/kotlin/org/knowledger/ledger/core/base/data/ByteEncodable.kt
+++ ledger-core/data/src/main/kotlin/org/knowledger/ledger/core/base/data/ByteEncodable.kt
@@ -1,5 +1,18 @@
 package org.knowledger.ledger.core.base.data
 
-interface ByteEncodable {
+interface ByteEncodable : Comparable<ByteEncodable> {
     val bytes: ByteArray
+
+    override fun compareTo(other: ByteEncodable): Int {
+        val bytesLeft = bytes
+        val bytesRight = other.bytes
+        var i = 0
+        var reduce = 0
+        while (i < bytesLeft.size && reduce == 0) {
+            reduce = bytesLeft[i].compareTo(bytesRight[i])
+            i++
+        }
+        return reduce
+    }
+
 }
