fb61e62c4c881165e0631354ff6bef978e269593
André Diogo <redan.blue27@gmail.com>
2020-07-05 21:15:41 +0100

    LedgerSerializer.kt operates over Immutable types.
    Decoding and Encoding are defined based on Format.

--- ledger/src/main/kotlin/org/knowledger/ledger/serial/SerializableAs.kt
+++ ledger/src/main/kotlin/org/knowledger/ledger/serial/SerializableAs.kt
@@ -1,50 +1,76 @@
 package org.knowledger.ledger.serial
 
+import kotlinx.serialization.DeserializationStrategy
+import kotlinx.serialization.SerializationStrategy
+import kotlinx.serialization.builtins.list
 import org.knowledger.collections.SortedList
-import org.knowledger.ledger.storage.Block
-import org.knowledger.ledger.storage.BlockHeader
-import org.knowledger.ledger.storage.Coinbase
-import org.knowledger.ledger.storage.MerkleTree
-import org.knowledger.ledger.storage.Transaction
-import org.knowledger.ledger.storage.Witness
+import org.knowledger.ledger.crypto.storage.ImmutableMerkleTree
+import org.knowledger.ledger.storage.block.ImmutableBlock
+import org.knowledger.ledger.storage.block.header.ImmutableBlockHeader
+import org.knowledger.ledger.storage.coinbase.ImmutableCoinbase
+import org.knowledger.ledger.storage.coinbase.header.ImmutableCoinbaseHeader
+import org.knowledger.ledger.storage.transaction.ImmutableTransaction
+import org.knowledger.ledger.storage.witness.ImmutableWitness
 
 interface SerializableAs<T> {
-    fun encodeBlock(block: Block): T
-    fun encodeBlockHeader(blockHeader: BlockHeader): T
-    fun encodeCoinbase(coinbase: Coinbase): T
-    fun encodeMerkleTree(merkleTree: MerkleTree): T
-    fun encodeTransaction(transaction: Transaction): T
-    fun encodeTransactions(transactions: SortedList<Transaction>): T
-    fun encodeTransactions(transactions: Iterable<Transaction>): T
-    fun encodeWitness(witness: Witness): T
+    fun <R> encode(strategy: SerializationStrategy<R>, element: R): T
+    fun <R> decode(strategy: DeserializationStrategy<R>, element: T): R
+
+    fun encodeBlock(block: ImmutableBlock): T =
+        encode(ImmutableBlock.serializer(), block)
+
+    fun encodeBlockHeader(blockHeader: ImmutableBlockHeader): T =
+        encode(ImmutableBlockHeader.serializer(), blockHeader)
+
+    fun encodeCoinbase(coinbase: ImmutableCoinbase): T =
+        encode(ImmutableCoinbase.serializer(), coinbase)
+
+    fun encodeCoinbaseHeader(coinbaseHeader: ImmutableCoinbaseHeader): T =
+        encode(ImmutableCoinbaseHeader.serializer(), coinbaseHeader)
+
+    fun encodeMerkleTree(merkleTree: ImmutableMerkleTree): T =
+        encode(ImmutableMerkleTree.serializer(), merkleTree)
+
+    fun encodeTransaction(transaction: ImmutableTransaction): T =
+        encode(ImmutableTransaction.serializer(), transaction)
+
+    fun encodeTransactions(transactions: SortedList<ImmutableTransaction>): T =
+        encode(SortedListSerializer(ImmutableTransaction.serializer()), transactions)
+
+    fun encodeTransactions(transactions: Iterable<ImmutableTransaction>): T =
+        encode(ImmutableTransaction.serializer().list, transactions.toList())
+
+    fun encodeWitness(witness: ImmutableWitness): T =
+        encode(ImmutableWitness.serializer(), witness)
 
 
-    fun encodeCompactBlock(block: Block): T
-    fun encodeCompactBlockHeader(blockHeader: BlockHeader): T
-    fun encodeCompactCoinbase(coinbase: Coinbase): T
-    fun encodeCompactMerkleTree(merkleTree: MerkleTree): T
-    fun encodeCompactTransaction(transaction: Transaction): T
-    fun encodeCompactTransactions(transactions: SortedList<Transaction>): T
-    fun encodeCompactTransactions(transactions: Iterable<Transaction>): T
-    fun encodeCompactWitness(witness: Witness): T
+    fun decodeBlock(block: T): ImmutableBlock =
+        decode(ImmutableBlock.serializer(), block)
 
+    fun decodeBlockHeader(blockHeader: T): ImmutableBlockHeader =
+        decode(ImmutableBlockHeader.serializer(), blockHeader)
 
-    fun decodeBlock(block: T): Block
-    fun decodeBlockHeader(blockHeader: T): BlockHeader
-    fun decodeCoinbase(coinbase: T): Coinbase
-    fun decodeMerkleTree(merkleTree: T): MerkleTree
-    fun decodeTransaction(transaction: T): Transaction
-    fun decodeTransactionsSet(transactions: T): SortedList<Transaction>
-    fun decodeTransactions(transactions: T): Iterable<Transaction>
-    fun decodeWitness(witness: T): Witness
+    fun decodeCoinbase(coinbase: T): ImmutableCoinbase =
+        decode(ImmutableCoinbase.serializer(), coinbase)
 
+    fun decodeCoinbaseHeader(coinbaseHeader: T): ImmutableCoinbaseHeader =
+        decode(ImmutableCoinbaseHeader.serializer(), coinbaseHeader)
 
-    fun decodeCompactBlock(block: T): Block
-    fun decodeCompactBlockHeader(blockHeader: T): BlockHeader
-    fun decodeCompactCoinbase(coinbase: T): Coinbase
-    fun decodeCompactMerkleTree(merkleTree: T): MerkleTree
-    fun decodeCompactTransaction(transaction: T): Transaction
-    fun decodeCompactTransactionsSet(transactions: T): SortedList<Transaction>
-    fun decodeCompactTransactions(transactions: T): Iterable<Transaction>
-    fun decodeCompactWitness(witness: T): Witness
+    fun decodeMerkleTree(merkleTree: T): ImmutableMerkleTree =
+        decode(ImmutableMerkleTree.serializer(), merkleTree)
+
+    fun decodeTransaction(transaction: T): ImmutableTransaction =
+        decode(ImmutableTransaction.serializer(), transaction)
+
+    fun decodeTransactionsSet(transactions: T): SortedList<ImmutableTransaction> =
+        decode(
+            SortedListSerializer(ImmutableTransaction.serializer()),
+            transactions
+        )
+
+    fun decodeTransactions(transactions: T): Iterable<ImmutableTransaction> =
+        decode(ImmutableTransaction.serializer().list, transactions)
+
+    fun decodeWitness(witness: T): ImmutableWitness =
+        decode(ImmutableWitness.serializer(), witness)
 }
