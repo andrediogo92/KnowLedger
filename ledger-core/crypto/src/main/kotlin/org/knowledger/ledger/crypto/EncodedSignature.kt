02b9a03a2abcc0763d36897962c23b79d8c3130c
André Diogo <redan.blue27@gmail.com>
2020-07-05 20:40:34 +0100

    Standardizes serializers through base64 encoding of byte arrays.

--- ledger-core/crypto/src/main/kotlin/org/knowledger/ledger/crypto/EncodedSignature.kt
+++ ledger-core/crypto/src/main/kotlin/org/knowledger/ledger/crypto/EncodedSignature.kt
@@ -2,9 +2,9 @@ package org.knowledger.ledger.crypto
 
 import kotlinx.serialization.Serializable
 import org.knowledger.ledger.core.base.data.ByteEncodable
-import org.knowledger.ledger.crypto.serial.EncodedSignatureByteSerializer
+import org.knowledger.ledger.crypto.serial.EncodedSignatureSerializer
 
-@Serializable(with = EncodedSignatureByteSerializer::class)
+@Serializable(with = EncodedSignatureSerializer::class)
 data class EncodedSignature(
     override val bytes: ByteArray
 ) : ByteEncodable {
