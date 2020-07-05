6ec0ab0adbaaacb823abf7933392f03905a5090e
André Diogo <redan.blue27@gmail.com>
2020-07-05 18:32:11 +0100

    Adds static size calculus for witnesses. Their size is fixed based on their schema.

---
+++ ledger/src/main/kotlin/org/knowledger/ledger/storage/StaticSizeCalculus.kt
@@ -0,0 +1,19 @@
+package org.knowledger.ledger.storage
+
+import org.knowledger.ledger.storage.witness.Witness
+
+
+internal fun Witness.calculateSize(hashSize: Int): Int =
+    transactionOutputs.calculateTransactionOutputsSize(hashSize) +
+            hashSize * 3 + publicKey.bytes.size + Int.SIZE_BYTES
+
+internal fun calculateTXOSize(hashSize: Int): Int =
+    Int.SIZE_BYTES * 2 + hashSize * 4
+
+internal fun Iterable<Witness>.calculateWitnessesSize(
+    hashSize: Int
+): Int = sumBy { it.calculateSize(hashSize) }
+
+internal fun Iterable<TransactionOutput>.calculateTransactionOutputsSize(
+    hashSize: Int
+): Int = sumBy { calculateTXOSize(hashSize) }
