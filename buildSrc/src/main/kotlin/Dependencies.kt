ba1bda2bb66fb7b1a5d1835476f4cf6514756c59
André Diogo <redan.blue27@gmail.com>
2020-07-09 20:03:40 +0100

    Updates dependencies and gradle version.

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,7 +1,7 @@
 object Versions {
     const val assertK = "0.22"
-    const val autoService = "1.0-rc6"
-    const val bouncyCastle = "1.65"
+    const val autoService = "1.0-rc7"
+    const val bouncyCastle = "1.66"
     const val commons = "1.3"
     const val dokka = "0.10.1"
     const val eclipsePaho = "1.2.4"
@@ -15,9 +15,9 @@ object Versions {
     const val jUnitRunner = "1.6.2"
     const val kotlin = "1.3.72"
     const val koin = "2.1.5"
-    const val kotlinPoet = "1.5.0"
+    const val kotlinPoet = "1.6.0"
     const val kotlinResult = "1.1.7"
-    const val orientDB = "3.0.31"
+    const val orientDB = "3.0.32"
     const val serial = "0.20.0"
     const val tinylog = "2.1.2"
 }
