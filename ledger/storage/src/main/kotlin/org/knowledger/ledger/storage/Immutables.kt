3a79f9d93ac096f05da382eeda29c5d2532d3f2f
André Diogo <redan.blue27@gmail.com>
2020-07-27 17:55:17 +0100

    Stabilizes immutable copies of every possible type.

--- ledger/storage/src/main/kotlin/org/knowledger/ledger/storage/Immutables.kt
+++ ledger/storage/src/main/kotlin/org/knowledger/ledger/storage/Immutables.kt
@@ -1,13 +1,15 @@
 package org.knowledger.ledger.storage
 
 import org.knowledger.collections.toMutableSortedListFromPreSorted
-import org.knowledger.ledger.config.ChainId
-import org.knowledger.ledger.config.chainid.ImmutableChainId
 import org.knowledger.ledger.crypto.storage.immutableCopy
 import org.knowledger.ledger.storage.block.ImmutableBlock
 import org.knowledger.ledger.storage.block.header.ImmutableBlockHeader
 import org.knowledger.ledger.storage.coinbase.ImmutableCoinbase
 import org.knowledger.ledger.storage.coinbase.header.ImmutableCoinbaseHeader
+import org.knowledger.ledger.storage.config.block.ImmutableBlockParams
+import org.knowledger.ledger.storage.config.chainid.ImmutableChainId
+import org.knowledger.ledger.storage.config.coinbase.ImmutableCoinbaseParams
+import org.knowledger.ledger.storage.config.ledger.ImmutableLedgerParams
 import org.knowledger.ledger.storage.transaction.ImmutableTransaction
 import org.knowledger.ledger.storage.transaction.output.ImmutableTransactionOutput
 import org.knowledger.ledger.storage.witness.ImmutableWitness
@@ -18,41 +20,61 @@ fun Block.immutableCopy(): ImmutableBlock =
             .map { it.immutableCopy() }
             .toMutableSortedListFromPreSorted(),
         coinbase = coinbase.immutableCopy(),
-        header = header.immutableCopy(),
+        blockHeader = blockHeader.immutableCopy(),
         merkleTree = merkleTree.immutableCopy(),
         approximateSize = approximateSize
     )
 
 fun BlockHeader.immutableCopy(): ImmutableBlockHeader =
     ImmutableBlockHeader(
-        chainId = chainId.immutableCopy(), merkleRoot = merkleRoot,
-        previousHash = previousHash, params = params,
+        chainHash = chainHash, merkleRoot = merkleRoot,
+        previousHash = previousHash, blockParams = blockParams.immutableCopy(),
         seconds = seconds, nonce = nonce, hash = hash
     )
 
+fun BlockParams.immutableCopy(): ImmutableBlockParams =
+    ImmutableBlockParams(
+        blockLength = blockLength, blockMemorySize = blockMemorySize
+    )
+
 fun ChainId.immutableCopy(): ImmutableChainId =
-    ImmutableChainId(tag, ledgerHash, hash)
+    ImmutableChainId(hash, ledgerHash, tag, blockParams, coinbaseParams)
 
 fun Coinbase.immutableCopy(): ImmutableCoinbase =
     ImmutableCoinbase(
-        header = header.immutableCopy(),
+        coinbaseHeader = coinbaseHeader.immutableCopy(),
         immutableWitnesses = witnesses
             .map { it.immutableCopy() }
             .toMutableSortedListFromPreSorted(),
         merkleTree = merkleTree.immutableCopy()
     )
 
+fun CoinbaseParams.immutableCopy(): ImmutableCoinbaseParams =
+    ImmutableCoinbaseParams(
+        hashSize = hashSize, timeIncentive = timeIncentive,
+        valueIncentive = valueIncentive, baseIncentive = baseIncentive,
+        dividingThreshold = dividingThreshold, formula = formula
+    )
+
 fun CoinbaseHeader.immutableCopy(): ImmutableCoinbaseHeader =
     ImmutableCoinbaseHeader(
-        hash = hash, payout = payout, coinbaseParams = coinbaseParams,
+        hash = hash, payout = payout,
+        coinbaseParams = coinbaseParams.immutableCopy(),
         merkleRoot = merkleRoot, difficulty = difficulty,
         blockheight = blockheight, extraNonce = extraNonce
+    )
+
+fun LedgerParams.immutableCopy(): ImmutableLedgerParams =
+    ImmutableLedgerParams(
+        hashers = hashers, recalculationTime = recalculationTime,
+        recalculationTrigger = recalculationTrigger
     )
 
 fun Transaction.immutableCopy(): ImmutableTransaction =
     ImmutableTransaction(
         hash = hash, signature = signature,
-        publicKey = publicKey, data = data
+        publicKey = publicKey, data = data,
+        approximateSize = approximateSize
     )
 
 fun TransactionOutput.immutableCopy(): ImmutableTransactionOutput =
