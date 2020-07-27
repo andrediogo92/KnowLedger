6f2a604d82986304cf6ee7d37340213b96c058fd
André Diogo <redan.blue27@gmail.com>
2020-07-27 18:00:39 +0100

    Adds an enum to adequately identify all storage types to be dynamically resolved.

---
+++ ledger/storage/src/main/kotlin/org/knowledger/ledger/storage/AdapterIndexes.kt
@@ -0,0 +1,24 @@
+package org.knowledger.ledger.storage
+
+enum class AdapterIds {
+    Block,
+    BlockHeader,
+    BlockParams,
+    ChainId,
+    ChainHandle,
+    Coinbase,
+    CoinbaseHeader,
+    CoinbaseParams,
+    Identity,
+    LedgerConfig,
+    LedgerData,
+    LedgerId,
+    LedgerParams,
+    MerkleTree,
+    PhysicalData,
+    PoolTransaction,
+    Transaction,
+    TransactionOutput,
+    TransactionPool,
+    Witness
+}
