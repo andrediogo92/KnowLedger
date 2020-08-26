6ee66ce7396eaa0ded5e06b20dfd16446feda7f1
André Diogo <redan.blue27@gmail.com>
2020-08-27 00:13:45 +0100

    Tidies db results. Adds no update to DataFailure.kt

--- ledger/core/db/src/main/kotlin/org/knowledger/ledger/database/results/QueryFailure.kt
+++ ledger/core/db/src/main/kotlin/org/knowledger/ledger/database/results/QueryFailure.kt
@@ -3,32 +3,17 @@ package org.knowledger.ledger.database.results
 import org.knowledger.ledger.results.Failable
 import org.knowledger.ledger.results.Failure
 
-
 sealed class QueryFailure : Failure {
-    class NonExistentData(
-        cause: String
-    ) : QueryFailure() {
-        override val failable: Failable.LightFailure =
-            Failable.LightFailure(
-                cause
-            )
-    }
+  class NonExistentData(cause: String) : QueryFailure() {
+    override val failable: Failable.LightFailure = Failable.LightFailure(cause)
+  }
 
+  class UnknownFailure(cause: String, exception: Exception?) : QueryFailure() {
+    override val failable: Failable.HardFailure = Failable.HardFailure(cause, exception)
+  }
 
-    class UnknownFailure(
-        cause: String,
-        exception: Exception?
-    ) : QueryFailure() {
-        override val failable: Failable.HardFailure =
-            Failable.HardFailure(cause, exception)
-    }
-
-    class Propagated(
-        pointOfFailure: String,
-        failable: Failable
-    ) : QueryFailure() {
-        override val failable: Failable.PropagatedFailure =
-            Failable.PropagatedFailure(pointOfFailure, failable)
-    }
-
+  class Propagated(pointOfFailure: String, failable: Failable) : QueryFailure() {
+    override val failable: Failable.PropagatedFailure =
+      Failable.PropagatedFailure(pointOfFailure, failable)
+  }
 }
