123a3188fea076328ba7e0be358f4112c02a653d
André Diogo <redan.blue27@gmail.com>
2020-08-26 23:44:36 +0100

    Moves default data formula to crypto module.
    Data formula is enhanced to be hashable and tracked at the LedgerConfigurationService.

---
+++ ledger/core/crypto/src/main/kotlin/org/knowledger/ledger/crypto/data/DefaultDiff.kt
@@ -0,0 +1,40 @@
+@file:UseSerializers(HashSerializer::class, HashAlgorithmSerializer::class)
+
+package org.knowledger.ledger.crypto.data
+
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.UseSerializers
+import org.knowledger.ledger.core.data.DataFormula
+import org.knowledger.ledger.core.data.Payout
+import org.knowledger.ledger.core.data.hash.Hash
+import org.knowledger.ledger.core.serial.HashSerializer
+import org.knowledger.ledger.crypto.Hashers
+import org.knowledger.ledger.crypto.hash.classDigest
+import org.knowledger.ledger.crypto.serial.HashAlgorithmSerializer
+import java.math.BigDecimal
+import java.math.MathContext
+
+@Serializable
+class DefaultDiff(val hashers: Hashers) : DataFormula {
+    override val hash: Hash = classDigest(hashers)
+
+    override fun calculateDiff(
+        base: Long, timeBase: Long, deltaTime: BigDecimal, valueBase: Long,
+        deltaValue: BigDecimal, constant: Long, threshold: Long, mathContext: MathContext,
+    ): Payout {
+        val standardDivisor = BigDecimal(threshold * constant)
+        val timeFactor = deltaTime
+            .multiply(BigDecimal(timeBase))
+            .pow(2, mathContext)
+            .divide(standardDivisor, mathContext)
+
+        val valueFactor = deltaValue
+            .divide(BigDecimal(2), mathContext)
+            .multiply(BigDecimal(valueBase))
+            .divide(standardDivisor, mathContext)
+
+        val baseFactor = BigDecimal(base).divide(standardDivisor, mathContext)
+        return Payout(timeFactor.add(valueFactor).add(baseFactor))
+    }
+
+}
--- ledger/core/data/src/main/kotlin/org/knowledger/ledger/core/data/DataFormula.kt
+++ ledger/core/data/src/main/kotlin/org/knowledger/ledger/core/data/DataFormula.kt
@@ -1,17 +1,15 @@
 package org.knowledger.ledger.core.data
 
+import org.knowledger.ledger.core.data.hash.Hashing
 import java.math.BigDecimal
 import java.math.MathContext
 
-interface DataFormula {
+interface DataFormula : Hashing, Comparable<DataFormula> {
+    override fun compareTo(other: DataFormula): Int =
+        hash.compareTo(other.hash)
+
     fun calculateDiff(
-        base: Long,
-        timeBase: Long,
-        deltaTime: BigDecimal,
-        valueBase: Long,
-        deltaValue: BigDecimal,
-        constant: Long,
-        threshold: Long,
-        mathContext: MathContext
+        base: Long, timeBase: Long, deltaTime: BigDecimal, valueBase: Long,
+        deltaValue: BigDecimal, constant: Long, threshold: Long, mathContext: MathContext,
     ): Payout
 }
