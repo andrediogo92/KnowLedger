082c468f84ba425983efda913e1fdaf7bebe9e98
André Diogo <redan.blue27@gmail.com>
2020-08-28 22:52:37 +0100

    Tidies up base64-extensions module.

--- base64-extensions/src/main/kotlin/org/knowledger/base64/Base64Functions.kt
+++ base64-extensions/src/main/kotlin/org/knowledger/base64/Base64Functions.kt
@@ -18,28 +18,20 @@ inline fun String.encodedStringInUTF8(): ByteArray = encodeToByteArray()
 @OptIn(ExperimentalStdlibApi::class)
 inline fun ByteArray.decodedUTF8String(): String = decodeToString()
 
-fun Key.base64Encoded(): Base64String =
-    encoded.base64Encoded()
+fun Key.base64Encoded(): Base64String = encoded.base64Encoded()
 
-fun ByteEncodable.base64Encoded(): Base64String =
-    bytes.base64Encoded()
+fun ByteEncodable.base64Encoded(): Base64String = bytes.base64Encoded()
 
-fun String.base64Encoded(): Base64String =
-    encodedStringInUTF8().base64Encoded()
+fun String.base64Encoded(): Base64String = encodedStringInUTF8().base64Encoded()
 
-fun ByteArray.base64Encoded(): Base64String =
-    b64Encoder.encodeToString(this)
+fun ByteArray.base64Encoded(): Base64String = b64Encoder.encodeToString(this)
 
-fun Base64String.base64Decoded(): ByteArray =
-    b64Decoder.decode(this)
+fun Base64String.base64Decoded(): ByteArray = b64Decoder.decode(this)
 
-fun Base64String.base64DecodedToHash(): Hash =
-    Hash(base64Decoded())
+fun Base64String.base64DecodedToHash(): Hash = Hash(base64Decoded())
 
-fun Base64String.base64DecodedToDifficulty(): Difficulty =
-    Difficulty(BigInteger(base64Decoded()))
+fun Base64String.base64DecodedToDifficulty(): Difficulty = Difficulty(BigInteger(base64Decoded()))
 
-fun Base64String.base64DecodedToUTF8(): String =
-    base64Decoded().decodedUTF8String()
+fun Base64String.base64DecodedToUTF8(): String = base64Decoded().decodedUTF8String()
 
 typealias Base64String = String
