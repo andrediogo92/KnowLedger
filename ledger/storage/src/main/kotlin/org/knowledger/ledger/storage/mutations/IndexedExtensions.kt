e6352567ec9ddc4afa283fc2a81f4ab3043234ad
André Diogo <redan.blue27@gmail.com>
2020-08-28 23:20:24 +0100

    Tidies mutation extension methods.

--- ledger/storage/src/main/kotlin/org/knowledger/ledger/storage/mutations/IndexedExtensions.kt
+++ ledger/storage/src/main/kotlin/org/knowledger/ledger/storage/mutations/IndexedExtensions.kt
@@ -2,35 +2,21 @@ package org.knowledger.ledger.storage.mutations
 
 import org.knowledger.collections.SortedList
 
-fun <T, R : Iterable<T>> R.indexed(): R where T : Comparable<T>,
-                                              T : Indexed =
+fun <T, R : Iterable<T>> R.indexed(): R where T : Comparable<T>, T : Indexed =
+    onEachIndexed { i, elem -> elem.markIndex(i) }
+
+fun <T, R : SortedList<T>> R.indexed(): R where T : Comparable<T>, T : Indexed =
+    onEachIndexed { i, elem -> elem.markIndex(i) }
+
+fun <T, R : SortedList<T>> R.indexed(from: Int): R where T : Comparable<T>, T : Indexed =
     apply {
-        forEachIndexed { i, elem ->
-            elem.markIndex(i)
-        }
+        val min = minOf(0, from)
+        subList(min, size).forEachIndexed { i, elem -> elem.markIndex(i) }
     }
 
-fun <T, R : SortedList<T>> R.indexed(): R where T : Comparable<T>,
-                                                T : Indexed =
+fun <T, R : SortedList<T>> R.indexed(from: Int, to: Int): R where T : Comparable<T>, T : Indexed =
     apply {
-        forEachIndexed { i, elem ->
-            elem.markIndex(i)
-        }
-    }
-
-fun <T, R : SortedList<T>> R.indexed(from: Int): R where T : Comparable<T>,
-                                                         T : Indexed =
-    apply {
-        for (i in from until size) {
-            this[i].markIndex(i)
-        }
-    }
-
-fun <T, R : SortedList<T>> R.indexed(from: Int, to: Int): R where T : Comparable<T>,
-                                                                  T : Indexed =
-    apply {
-        val min = minOf(to, size)
-        for (i in from until min) {
-            this[i].markIndex(i)
-        }
+        val min = minOf(0, from)
+        val max = minOf(to, size)
+        subList(min, max).forEachIndexed { i, elem -> elem.markIndex(i) }
     }
--- ledger/storage/src/main/kotlin/org/knowledger/ledger/storage/mutations/StaticSizeCalculus.kt
+++ ledger/storage/src/main/kotlin/org/knowledger/ledger/storage/mutations/StaticSizeCalculus.kt
@@ -6,15 +6,12 @@ import org.knowledger.ledger.storage.witness.Witness
 
 internal fun Witness.calculateSize(hashSize: Int): Int =
     transactionOutputs.calculateTransactionOutputsSize(hashSize) +
-            hashSize * 3 + publicKey.bytes.size + Int.SIZE_BYTES
+    hashSize * 3 + publicKey.bytes.size + Int.SIZE_BYTES
 
-internal fun calculateTXOSize(hashSize: Int): Int =
-    Int.SIZE_BYTES * 2 + hashSize * 4
+internal fun calculateTXOSize(hashSize: Int): Int = Int.SIZE_BYTES * 2 + hashSize * 4
 
-internal fun Iterable<Witness>.calculateWitnessesSize(
-    hashSize: Int
-): Int = sumBy { it.calculateSize(hashSize) }
+internal fun Iterable<Witness>.calculateWitnessesSize(hashSize: Int): Int =
+    sumBy { it.calculateSize(hashSize) }
 
-internal fun Iterable<TransactionOutput>.calculateTransactionOutputsSize(
-    hashSize: Int
-): Int = sumBy { calculateTXOSize(hashSize) }
+internal fun Iterable<TransactionOutput>.calculateTransactionOutputsSize(hashSize: Int): Int =
+    sumBy { calculateTXOSize(hashSize) }
