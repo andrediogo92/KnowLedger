cdee2e37caf0413096032d48cab6fb0016cce235
André Diogo <redan.blue27@gmail.com>
2020-08-28 23:19:40 +0100

    Tidies remaining config classes.

--- ledger/storage/src/main/kotlin/org/knowledger/ledger/storage/config/GlobalLedgerConfiguration.kt
+++ ledger/storage/src/main/kotlin/org/knowledger/ledger/storage/config/GlobalLedgerConfiguration.kt
@@ -7,10 +7,7 @@ import java.math.RoundingMode
 
 object GlobalLedgerConfiguration {
     const val CACHE_SIZE = 40
-    val GLOBALCONTEXT =
-        MathContext(15, RoundingMode.HALF_EVEN)
-    val RECALC_DIV =
-        BigInteger("10000000000000")
-    val RECALC_MULT =
-        BigDecimal("10000000000000")
+    val GLOBALCONTEXT = MathContext(15, RoundingMode.HALF_EVEN)
+    val RECALC_DIV = BigInteger("10000000000000")
+    val RECALC_MULT = BigDecimal("10000000000000")
 }
--- ledger/storage/src/main/kotlin/org/knowledger/ledger/storage/config/LedgerId.kt
+++ ledger/storage/src/main/kotlin/org/knowledger/ledger/storage/config/LedgerId.kt
@@ -1,46 +1,43 @@
 @file:UseSerializers(
-    InstantSerializer::class, UUIDSerializer::class,
-    HashSerializer::class, HashAlgorithmSerializer::class
+    InstantSerializer::class, UUIDSerializer::class, HashSerializer::class
 )
 
 package org.knowledger.ledger.storage.config
 
 import kotlinx.serialization.BinaryFormat
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.UseSerializers
+import org.knowledger.ledger.core.Instant
+import org.knowledger.ledger.core.UUID
 import org.knowledger.ledger.core.calculateHash
+import org.knowledger.ledger.core.nowUTC
 import org.knowledger.ledger.core.serial.HashSerializer
 import org.knowledger.ledger.core.serial.InstantSerializer
 import org.knowledger.ledger.core.serial.UUIDSerializer
 import org.knowledger.ledger.crypto.Hash
 import org.knowledger.ledger.crypto.Hashers
 import org.knowledger.ledger.crypto.Hashing
-import org.knowledger.ledger.crypto.serial.HashAlgorithmSerializer
 import org.knowledger.ledger.storage.HashSerializable
 import org.knowledger.ledger.storage.LedgerContract
 import org.knowledger.ledger.storage.LedgerParams
 import org.knowledger.ledger.storage.config.ledger.ImmutableLedgerParams
 import org.knowledger.ledger.storage.immutableCopy
-import java.time.Instant
-import java.util.*
 
+@OptIn(ExperimentalSerializationApi::class)
 data class LedgerId(
-    val tag: String, override val hash: Hash,
-    val uuid: UUID, val instant: Instant, val ledgerParams: LedgerParams
+    val tag: String, override val hash: Hash, val uuid: UUID,
+    val instant: Instant, val ledgerParams: LedgerParams,
 ) : Hashing, LedgerContract {
     constructor(
-        tag: String, hashers: Hashers,
-        encoder: BinaryFormat, ledgerParams: LedgerParams
+        tag: String, hashers: Hashers, encoder: BinaryFormat, ledgerParams: LedgerParams,
     ) : this(
         tag, encoder, Builder(
-            tag, hashers, UUID.randomUUID(),
-            Instant.now(), ledgerParams.immutableCopy()
+            tag, hashers, UUID.randomUUID(), nowUTC(), ledgerParams.immutableCopy()
         )
     )
 
-    private constructor(
-        tag: String, encoder: BinaryFormat, builder: Builder
-    ) : this(
+    private constructor(tag: String, encoder: BinaryFormat, builder: Builder) : this(
         tag, builder.calculateHash(builder.hashers, encoder),
         builder.uuid, builder.instant, builder.ledgerParams
     )
@@ -48,9 +45,9 @@ data class LedgerId(
     @Serializable
     private data class Builder(
         val tag: String, val hashers: Hashers, val uuid: UUID,
-        val instant: Instant, val ledgerParams: ImmutableLedgerParams
+        val instant: Instant, val ledgerParams: ImmutableLedgerParams,
     ) : HashSerializable {
         override fun serialize(encoder: BinaryFormat): ByteArray =
-            encoder.dump(serializer(), this)
+            encoder.encodeToByteArray(serializer(), this)
     }
 }
