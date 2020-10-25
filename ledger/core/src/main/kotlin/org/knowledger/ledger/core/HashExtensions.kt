fc2874baec1e60e11fbe49d52c5145368c9fb602
André Diogo <redan.blue27@gmail.com>
2020-10-25 23:44:15 +0000

    Moves all conversions over fundamental types to top-level core module.

--- ledger/core/src/main/kotlin/org/knowledger/ledger/core/HashExtensions.kt
+++ ledger/core/src/main/kotlin/org/knowledger/ledger/core/HashExtensions.kt
@@ -3,9 +3,15 @@ package org.knowledger.ledger.core
 
 import kotlinx.serialization.BinaryFormat
 import kotlinx.serialization.ExperimentalSerializationApi
+import org.knowledger.encoding.base32.base32Decoded
+import org.knowledger.encoding.base32.base32Encoded
+import org.knowledger.encoding.trimBaseNPadding
+import org.knowledger.ledger.core.adapters.Tag
+import org.knowledger.ledger.core.data.Difficulty
 import org.knowledger.ledger.core.data.HashSerializable
 import org.knowledger.ledger.crypto.Hash
 import org.knowledger.ledger.crypto.hash.Hashers
+import java.math.BigInteger
 
 fun <T : HashSerializable> T.calculateHash(hasher: Hashers, encoder: BinaryFormat): Hash =
     digest(hasher, encoder)
@@ -23,3 +29,16 @@ fun <T : HashSerializable> T.calculateApproximateSize(hasher: Hashers, encoder: 
         val hash = hasher.applyHash(this)
         size + hash.bytes.size
     }
+
+
+fun Hash.toDifficulty(): Difficulty =
+    Difficulty(BigInteger(bytes))
+
+fun Hash.toTag(): Tag =
+    Tag(base32Encoded().trimBaseNPadding())
+
+fun Tag.toHash(): Hash =
+    Hash(id.base32Decoded())
+
+fun Difficulty.toHash(): Hash =
+    Hash(bytes)
