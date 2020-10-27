2f424f73d694466e985965997f687d52f98f7783
André Diogo <redan.blue27@gmail.com>
2020-10-27 17:18:40 +0000

    Updates included modules for gradle build for kserial rename to data-serial.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,7 @@ rootProject.name = "Knowledger"
 include(
     "agent", "agent:ontologies", "agent:publish", "annotations", "benchmarks",
     "collections-extensions", "encoding-extensions", "example", "ledger", "ledger:core",
-    "ledger:core:crypto", "ledger:core:data", "ledger:core:db", "ledger:core:kserial",
+    "ledger:core:crypto", "ledger:core:data", "ledger:core:db", "ledger:core:data-serial",
     "ledger:orient", "ledger:storage", "generation", "results", "testing"
 )
 
